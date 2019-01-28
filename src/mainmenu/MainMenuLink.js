16213cc2e4e2b529e9d753c92b98fcbcdaff0d0e
Shmuel Melamud <smelamud@redhat.com>
2019-01-29 00:52:12 +0200

    Tiny refactoring.

--- src/mainmenu/MainMenuLink.js
+++ src/mainmenu/MainMenuLink.js
@@ -6,6 +6,6 @@ const MainMenuLink = ({rootLocation, href, children}) => (
     <Nav.Link href={rootLocation + href}>{children}</Nav.Link>
 );
 
-const mapStateToProps = (state) => ({rootLocation: state.root.location});
-
-export default connect(mapStateToProps)(MainMenuLink);
+export default connect(
+    (state) => ({rootLocation: state.root.location})
+)(MainMenuLink);
