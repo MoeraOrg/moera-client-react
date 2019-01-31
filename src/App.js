922d21e06815c55fb6c9147758ad7ca05385df87
Shmuel Melamud <smelamud@redhat.com>
2019-01-31 13:23:02 +0200

    Move all project modules to src/node_modules instead of modifying NODE_PATH.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import MainMenu from "mainmenu/MainMenu";
-import "App.css";
+import "./App.css";
 
 const App = () => (
     <>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import store from "store";
+import store from "./store";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "App";
+import App from "./App";
 
-import * as serviceWorker from "serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <Provider store={store}>
