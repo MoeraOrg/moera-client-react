7c80d6f0bc8324b98efce9fe6565990dd7002290
Shmuel Melamud <smelamud@redhat.com>
2019-02-13 12:00:12 +0200

    Eliminated possible race condition between the client and the browser extension content script.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
 
-fetch("http://moera.please.start.com/"); // Call the browser extension to inject communication code
 ReactDOM.render(
     <Provider store={store}>
         <App />
