94b6f207b9202cf492577424da8be6e05f2dc39d
Shmuel Melamud <smelamud@redhat.com>
2019-03-12 00:02:38 +0200

    The Great Move: All files sorted by api/, state/ and ui/ subdirectories.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import store from "./store";
+import store from "state/store";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "./App";
+import App from "ui/App";
 
 import * as serviceWorker from "./serviceWorker";
 
