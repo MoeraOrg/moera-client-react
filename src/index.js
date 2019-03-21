b48330390adae1c728e26dc044a0c8820046f18d
Shmuel Melamud <smelamud@redhat.com>
2019-03-22 00:11:39 +0200

    If the profile page is not active, connect/disconnect cause the profile to be erased and loaded on the next profile page activation. Load the profile on GO_TO_PAGE event, do not rely on componentDidMount() methods, because they are not called on page switch.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import store from "state/store";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "ui/App";
+import { goToPage } from "state/navigation/actions";
 
 import * as serviceWorker from "./serviceWorker";
 
@@ -15,6 +16,7 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("app-root")
 );
+store.dispatch(goToPage("index"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
