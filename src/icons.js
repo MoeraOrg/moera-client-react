1f5b12d3f8e0ef99be1a66792d36173384096591
Shmuel Melamud <smelamud@redhat.com>
2019-07-08 01:13:04 +0300

    Build/transform posting virtual page path. Display error if posting is not found.

--- src/icons.js
+++ src/icons.js
@@ -1,6 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
 
 export default function initIconLibrary() {
     library.add(faExclamationTriangle);
+    library.add(faFrown);
 }
