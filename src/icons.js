fba3b336c40b0a60b74689b4986b491fb39b389e
Shmuel Melamud <smelamud@redhat.com>
2019-08-20 23:26:19 +0300

    Hide "publishAt" input under clickable icon.

--- src/icons.js
+++ src/icons.js
@@ -1,7 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
 
 export default function initIconLibrary() {
+    library.add(faClock);
     library.add(faExclamationTriangle);
     library.add(faFrown);
 }
