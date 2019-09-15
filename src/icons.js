acbe78efc360d68863c7466f5a6bb63750c38639
Shmuel Melamud <smelamud@redhat.com>
2019-09-15 17:03:24 +0300

    Use chevron instead of ellipsis for posting menu and highlight it when hovering.

--- src/icons.js
+++ src/icons.js
@@ -1,9 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faClock, faEllipsisV, faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faClock, faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
 
 export default function initIconLibrary() {
+    library.add(faChevronDown);
     library.add(faClock);
-    library.add(faEllipsisV);
     library.add(faExclamationTriangle);
     library.add(faFrown);
 }
