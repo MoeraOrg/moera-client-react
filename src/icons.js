eb48b33f2733812623814d0f180b0d5ed58d44a7
Shmuel Melamud <smelamud@redhat.com>
2019-09-18 18:13:10 +0300

    Hide body format selection under icon.

--- src/icons.js
+++ src/icons.js
@@ -1,9 +1,22 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronDown, faClock, faExclamationTriangle, faFrown } from '@fortawesome/free-solid-svg-icons';
+import {
+    faChevronDown,
+    faClock,
+    faCode,
+    faExclamationTriangle,
+    faFileAlt,
+    faFrown,
+    faRemoveFormat
+} from '@fortawesome/free-solid-svg-icons';
+import { faMarkdown } from '@fortawesome/free-brands-svg-icons';
 
 export default function initIconLibrary() {
     library.add(faChevronDown);
     library.add(faClock);
+    library.add(faCode);
     library.add(faExclamationTriangle);
+    library.add(faFileAlt);
     library.add(faFrown);
+    library.add(faMarkdown);
+    library.add(faRemoveFormat);
 }
