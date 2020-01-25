663ed6b1d6f9baa45b377212b7c0a5a85dabe89d
Shmuel Melamud <smelamud@redhat.com>
2020-01-25 21:10:42 +0200

    ReactionsListView extracted from ReactionsDialog.

--- src/ui/reactionsdialog/ReactionsDialog.js
+++ src/ui/reactionsdialog/ReactionsDialog.js
@@ -1,55 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import cx from 'classnames';
 
-import { Loading, ModalDialog, NodeName, Twemoji } from "ui/control";
-import { closeReactionsDialog, reactionsDialogSelectTab } from "state/reactionsdialog/actions";
-import {
-    getReactionsDialogItems,
-    getReactionsDialogRemainingCount,
-    isReactionsDialogReactionsLoading
-} from "state/reactionsdialog/selectors";
+import { closeReactionsDialog } from "state/reactionsdialog/actions";
+import { ModalDialog } from "ui/control";
+import ReactionsListView from "ui/reactionsdialog/ReactionsListView";
 import "./ReactionsDialog.css";
-import ReactionVerifyButton from "ui/reactionsdialog/ReactionVerifyButton";
-
-const TotalsTabsImpl = ({loading, loaded, total, emojis, activeTab, reactionsDialogSelectTab}) => (
-    <>
-        {loaded &&
-            <div className="total-tabs">
-                <div className={cx("total", {"active": activeTab == null})}
-                     onClick={() => reactionsDialogSelectTab(null)}>
-                    All {total}
-                </div>
-                {emojis.map(rt =>
-                    <div key={rt.emoji} className={cx("total", {"active": activeTab === rt.emoji})}
-                         onClick={() => reactionsDialogSelectTab(rt.emoji)}>
-                        <Twemoji code={rt.emoji}/>{" "}{rt.total}
-                    </div>
-                )}
-            </div>
-        }
-        <Loading active={loading}/>
-    </>
-);
-
-const TotalsTabs = connect(
-    state => ({
-        ...state.reactionsDialog.totals,
-        activeTab: state.reactionsDialog.activeTab
-    }),
-    { reactionsDialogSelectTab }
-)(TotalsTabsImpl);
 
 class ReactionsDialog extends React.PureComponent {
+    #itemsDom;
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (((!prevProps.show && this.props.show) || prevProps.activeTab !== this.props.activeTab) && this.itemsNode) {
-            this.itemsNode.focus();
+        if (((!prevProps.show && this.props.show) || prevProps.activeTab !== this.props.activeTab) && this.#itemsDom) {
+            this.#itemsDom.focus();
         }
     }
 
     render() {
-        const {show, postingId, remaining, reactionsLoading, reactions, closeReactionsDialog} = this.props;
+        const {show, postingId, closeReactionsDialog} = this.props;
 
         if (!show) {
             return null;
@@ -58,24 +25,7 @@ class ReactionsDialog extends React.PureComponent {
         return (
             <ModalDialog onClose={closeReactionsDialog}>
                 <div className="reactions-dialog modal-body">
-                    <div className="totals clearfix">
-                        <TotalsTabs/>
-                        <button type="button" className="close" onClick={closeReactionsDialog}>&times;</button>
-                    </div>
-                    <div className="items" tabIndex="-1" ref={dom => {this.itemsNode = dom}}>
-                        {reactions.map(r =>
-                            <div className="item" key={r.moment}>
-                                <div className="emoji"><Twemoji code={r.emoji}/></div>
-                                <div className="owner-name">
-                                    <NodeName name={r.ownerName}/>
-                                    {" "}
-                                    <ReactionVerifyButton postingId={postingId} ownerName={r.ownerName}/>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                    {remaining > 0 && !reactionsLoading && <div className="more">{remaining} more</div>}
-                    <Loading active={reactionsLoading}/>
+                    <ReactionsListView postingId={postingId} itemsRef={dom => {this.#itemsDom = dom}}/>
                 </div>
             </ModalDialog>
         );
@@ -86,11 +36,7 @@ class ReactionsDialog extends React.PureComponent {
 export default connect(
     state => ({
         show: state.reactionsDialog.show,
-        postingId: state.reactionsDialog.postingId,
-        activeTab: state.reactionsDialog.activeTab,
-        remaining: getReactionsDialogRemainingCount(state),
-        reactionsLoading: isReactionsDialogReactionsLoading(state),
-        reactions: getReactionsDialogItems(state)
+        postingId: state.reactionsDialog.postingId
     }),
     { closeReactionsDialog }
 )(ReactionsDialog);
---
+++ src/ui/reactionsdialog/ReactionsListView.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import cx from 'classnames';
+
+import { closeReactionsDialog, reactionsDialogSelectTab } from "state/reactionsdialog/actions";
+import { Loading, NodeName, Twemoji } from "ui/control";
+import ReactionVerifyButton from "ui/reactionsdialog/ReactionVerifyButton";
+import {
+    getReactionsDialogItems,
+    getReactionsDialogRemainingCount,
+    isReactionsDialogReactionsLoading
+} from "state/reactionsdialog/selectors";
+
+const TotalsTabsImpl = ({loading, loaded, total, emojis, activeTab, reactionsDialogSelectTab}) => (
+    <>
+        {loaded &&
+        <div className="total-tabs">
+            <div className={cx("total", {"active": activeTab == null})}
+                 onClick={() => reactionsDialogSelectTab(null)}>
+                All {total}
+            </div>
+            {emojis.map(rt =>
+                <div key={rt.emoji} className={cx("total", {"active": activeTab === rt.emoji})}
+                     onClick={() => reactionsDialogSelectTab(rt.emoji)}>
+                    <Twemoji code={rt.emoji}/>{" "}{rt.total}
+                </div>
+            )}
+        </div>
+        }
+        <Loading active={loading}/>
+    </>
+);
+
+const TotalsTabs = connect(
+    state => ({
+        ...state.reactionsDialog.totals,
+        activeTab: state.reactionsDialog.activeTab
+    }),
+    { reactionsDialogSelectTab }
+)(TotalsTabsImpl);
+
+const ReactionsListView = ({postingId, itemsRef, remaining, reactionsLoading, reactions,
+                                   closeReactionsDialog}) => (
+    <>
+        <div className="totals clearfix">
+            <TotalsTabs/>
+            <button type="button" className="close" onClick={closeReactionsDialog}>&times;</button>
+        </div>
+        <div className="items" tabIndex="-1" ref={itemsRef}>
+            {reactions.map(r =>
+                <div className="item" key={r.moment}>
+                    <div className="emoji"><Twemoji code={r.emoji}/></div>
+                    <div className="owner-name">
+                        <NodeName name={r.ownerName}/>
+                        {" "}
+                        <ReactionVerifyButton postingId={postingId} ownerName={r.ownerName}/>
+                    </div>
+                </div>
+            )}
+        </div>
+        {remaining > 0 && !reactionsLoading && <div className="more">{remaining} more</div>}
+        <Loading active={reactionsLoading}/>
+    </>
+);
+
+export default connect(
+    state => ({
+        postingId: state.reactionsDialog.postingId,
+        remaining: getReactionsDialogRemainingCount(state),
+        reactionsLoading: isReactionsDialogReactionsLoading(state),
+        reactions: getReactionsDialogItems(state)
+    }),
+    { closeReactionsDialog }
+)(ReactionsListView);
