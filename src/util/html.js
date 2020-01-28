4458daa8286c5c34acdc700f7a29ae58273f84a3
Shmuel Melamud <smelamud@redhat.com>
2020-01-28 22:24:11 +0200

    Replace emojis in posts with Twemoji images.

--- src/util/html.js
+++ src/util/html.js
@@ -1,14 +1,32 @@
 import sanitizeHtml from 'sanitize-html';
+import { parse as parseEmojis } from 'twemoji-parser';
+
+export function replaceEmojis(html) {
+    if (!html) {
+        return "";
+    }
+
+    const entities = parseEmojis(html);
+    let shift = 0;
+    let current = html;
+    for (let entity of entities) {
+        const img = `<img src="${entity.url}" alt="${entity.text}" style="width: 1em; height: 1em">`;
+        current = current.substring(0, entity.indices[0] + shift) + img + current.substring(entity.indices[1] + shift);
+        shift += img.length - (entity.indices[1] - entity.indices[0]);
+    }
+
+    return current;
+}
 
 export function safePreviewHtml(html) {
     if (!html) {
         return "";
     }
-    return sanitizeHtml(html, {
+    return sanitizeHtml(replaceEmojis(html), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
         allowedAttributes: {
             ...sanitizeHtml.defaults.allowedAttributes,
-            img: ["src", "srcset", "width", "height"]
+            img: ["src", "srcset", "width", "height", "style"]
         },
         transformTags: {
             "h1": "b",
@@ -24,11 +42,11 @@ export function safeHtml(html) {
     if (!html) {
         return "";
     }
-    return sanitizeHtml(html, {
+    return sanitizeHtml(replaceEmojis(html), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["h1", "h2", "img"]),
         allowedAttributes: {
             ...sanitizeHtml.defaults.allowedAttributes,
-            img: ["src", "srcset", "width", "height"]
+            img: ["src", "srcset", "width", "height", "style"]
         }
     });
 }
