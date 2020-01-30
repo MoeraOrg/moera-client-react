e5a42cf12911373495b4b4b95f8d51a78a2f5105
Shmuel Melamud <smelamud@redhat.com>
2020-01-31 00:59:13 +0200

    Include into the app SVGs for all standard reactions.

--- src/ui/control/Twemoji.js
+++ src/ui/control/Twemoji.js
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import "./Twemoji.css";
 
+const CACHED = new Set(["23f3", "2694", "2708", "1f379", "1f37f", "1f381", "1f3a9", "1f44d", "1f44e", "1f47f",
+                        "1f48e", "1f494", "1f4a1", "1f4a4", "1f4a9", "1f4af", "1f600", "1f60d", "1f61c", "1f620",
+                        "1f622", "1f62b", "1f62d", "1f62e", "1f921", "1f926", "1f92e", "1f9f8"]);
+
 export const Twemoji = ({code, title = ""}) => {
     const emoji = typeof(code) === "string" ? code : Number(code).toString(16);
     switch (emoji) {
@@ -11,9 +15,11 @@ export const Twemoji = ({code, title = ""}) => {
             return <FontAwesomeIcon icon="thumbs-up" color="#2078f4" title={title}/>;
         case "1f44e":
             return <FontAwesomeIcon icon="thumbs-down" color="#2078f4" title={title}/>;
-        default:
-            return <img className="emoji" src={`https://twemoji.maxcdn.com/v/latest/svg/${emoji}.svg`}
-                        alt={title} title={title}/>
+        default: {
+            const src = CACHED.has(emoji)
+                ? `twemoji/${emoji}.svg` : `https://twemoji.maxcdn.com/v/latest/svg/${emoji}.svg`;
+            return <img className="emoji" src={src} alt={title} title={title}/>
+        }
     }
 };
 
