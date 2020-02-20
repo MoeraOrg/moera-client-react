6961dd0ec2804344952f10c49af6adcc23273969
Shmuel Melamud <smelamud@redhat.com>
2020-02-20 20:29:36 +0200

    Prevent calendar button crash when the Timeline is positioned on a pinned post.

--- src/state/timeline/selectors.js
+++ src/state/timeline/selectors.js
@@ -3,6 +3,8 @@ import moment from 'moment';
 import { isPermitted } from "state/node/selectors";
 import { isHomeOwnerNameSet } from "state/home/selectors";
 
+const MAX_MOMENT = 25337597040000; // January 1, 9999
+
 export function isTimelineGeneralToBeLoaded(state) {
     return !state.timeline.loadedGeneral && !state.timeline.loadingGeneral;
 }
@@ -21,7 +23,7 @@ export function getTimelineAt(state) {
 }
 
 export function getTimelineAtTimestamp(state) {
-    return state.timeline.at < Number.MAX_SAFE_INTEGER ? Math.floor(state.timeline.at / 100): moment().unix()
+    return state.timeline.at < MAX_MOMENT ? Math.floor(state.timeline.at / 100): moment().unix()
 }
 
 export function isTimelineContainsMoment(state, moment) {
