527683864d3fccde8ce68806133cbe7d3b109eb5
Shmuel Melamud <smelamud@redhat.com>
2020-03-15 03:32:08 +0200

    Do not trigger DISCONNECTED_FROM_HOME when already disconnected.

--- src/state/home/triggers.js
+++ src/state/home/triggers.js
@@ -1,4 +1,4 @@
-import { inv, trigger } from "state/trigger";
+import { conj, inv, trigger } from "state/trigger";
 import {
     CONNECTED_TO_HOME,
     DISCONNECT_FROM_HOME,
@@ -7,13 +7,13 @@ import {
     homeOwnerVerify
 } from "state/home/actions";
 import { EVENT_HOME_NODE_NAME_CHANGED } from "api/events/actions";
-import { hasInactiveConnections } from "state/home/selectors";
+import { hasInactiveConnections, isConnectedToHome } from "state/home/selectors";
 
 export default [
     trigger(CONNECTED_TO_HOME, true, homeOwnerVerify),
     trigger(
         DISCONNECT_FROM_HOME,
-        inv(hasInactiveConnections),
+        conj(isConnectedToHome, inv(hasInactiveConnections)),
         signal => disconnectedFromHome(signal.payload.location, signal.payload.login)
     ),
     trigger(EVENT_HOME_NODE_NAME_CHANGED, true, signal => homeOwnerSet(signal.payload.name)),
