d7d4078e76f464ddcf17a24775dd264904d3cf4e
Shmuel Melamud <smelamud@redhat.com>
2020-05-04 23:54:35 +0300

    Mark instants as viewed if they are loaded later than the popover becomes visible.

--- src/ui/mainmenu/connectionstatus/InstantButton.js
+++ src/ui/mainmenu/connectionstatus/InstantButton.js
@@ -9,10 +9,21 @@ import { getFeedState } from "state/feeds/selectors";
 
 class InstantButton extends React.PureComponent {
 
+    #visible;
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.viewAll();
+    }
+
     onToggle = (visible) => {
+        this.#visible = visible;
+        this.viewAll();
+    }
+
+    viewAll() {
         const {stories, feedStatusUpdate} = this.props;
 
-        if (!visible || stories == null || stories.length === 0 || stories[0].viewed) {
+        if (!this.#visible || stories == null || stories.length === 0 || stories[0].viewed) {
             return;
         }
         feedStatusUpdate(":instant", true, null, stories[0].moment);
