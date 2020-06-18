1754a201732c934e50268a29fd86f33d79e5712e
Shmuel Melamud <smelamud@redhat.com>
2020-06-19 00:34:35 +0300

    If detailed view of a posting was opened from the News feed, show button for returning to the feed.

--- src/ui/detailedposting/DetailedPosting.js
+++ src/ui/detailedposting/DetailedPosting.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { Button } from "ui/control";
 import { getPostingFeedReference } from "state/postings/selectors";
-import { goToTimeline } from "state/navigation/actions";
+import { goToNews, goToTimeline } from "state/navigation/actions";
 import { isConnectedToHome } from "state/home/selectors";
 import { isPermitted } from "state/node/selectors";
 import PostingMenu from "ui/posting/PostingMenu";
@@ -17,43 +17,55 @@ import PostingReactions from "ui/posting/PostingReactions";
 import PostingButtons from "ui/posting/PostingButtons";
 import PostingHtml from "ui/posting/PostingHtml";
 
-const DetailedPosting = ({posting, deleting, connectedToHome, isPermitted, goToTimeline}) => {
-    const story = getPostingFeedReference(posting, "timeline");
-    return (
-        <>
-            {story != null &&
-                <Button variant="outline-secondary" size="sm"
-                        onClick={() => goToTimeline(story.moment)}>&larr; Timeline</Button>
-            }
+const DetailedPostingImpl = ({story, posting, deleting, connectedToHome, isPermitted, feedTitle, goToFeed}) => (
+    <>
+        {story != null &&
+            <Button variant="outline-secondary" size="sm" onClick={() => goToFeed(story.moment)}>
+                &larr; {feedTitle}
+            </Button>
+        }
 
-            <div className="posting mt-2">
-                {deleting ?
-                    <PostingDeleting/>
-                :
-                    <>
-                        <PostingMenu posting={posting} story={story} isPermitted={isPermitted}/>
-                        <PostingPin pinned={story != null && story.pinned}/>
-                        <div className="owner-line">
-                            <PostingOwner posting={posting}/>
-                            <PostingDate id={posting.id}
-                                         publishedAt={story != null ? story.publishedAt : posting.createdAt}/>
-                            <PostingUpdated posting={posting}/>
-                        </div>
-                        <PostingSubject posting={posting} preview={false}/>
-                        <PostingHtml className="content" html={posting.body.text}/>
-                        <PostingReactions posting={posting}/>
-                        {connectedToHome && <PostingButtons posting={posting}/>}
-                    </>
-                }
-            </div>
-        </>
-    );
-};
+        <div className="posting mt-2">
+            {deleting ?
+                <PostingDeleting/>
+            :
+                <>
+                    <PostingMenu posting={posting} story={story} isPermitted={isPermitted}/>
+                    <PostingPin pinned={story != null && story.pinned}/>
+                    <div className="owner-line">
+                        <PostingOwner posting={posting}/>
+                        <PostingDate id={posting.id}
+                                     publishedAt={story != null ? story.publishedAt : posting.createdAt}/>
+                        <PostingUpdated posting={posting}/>
+                    </div>
+                    <PostingSubject posting={posting} preview={false}/>
+                    <PostingHtml className="content" html={posting.body.text}/>
+                    <PostingReactions posting={posting}/>
+                    {connectedToHome && <PostingButtons posting={posting}/>}
+                </>
+            }
+        </div>
+    </>
+);
+
+const DetailedPosting = ({posting, deleting, connectedToHome, isPermitted, goToTimeline, goToNews}) => {
+    let story = getPostingFeedReference(posting, "timeline");
+    if (story != null) {
+        return <DetailedPostingImpl story={story} posting={posting} deleting={deleting}
+                                    connectedToHome={connectedToHome} isPermitted={isPermitted} feedTitle="Timeline"
+                                    goToFeed={goToTimeline}/>
+    } else {
+        story = getPostingFeedReference(posting, "news");
+        return <DetailedPostingImpl story={story} posting={posting} deleting={deleting}
+                                    connectedToHome={connectedToHome} isPermitted={isPermitted} feedTitle="News"
+                                    goToFeed={goToNews}/>
+    }
+}
 
 export default connect(
     state => ({
         connectedToHome: isConnectedToHome(state),
         isPermitted: (operation, posting) => isPermitted(operation, posting, state)
     }),
-    { goToTimeline }
+    { goToTimeline, goToNews }
 )(DetailedPosting);
