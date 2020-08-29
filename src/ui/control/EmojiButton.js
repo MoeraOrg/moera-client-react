c4afd02daf58bda3838f8da46dfbeda019addde9
Shmuel Melamud <smelamud@redhat.com>
2020-08-30 00:51:12 +0300

    Set emoji button color on <button> tag level to set a correct text decoration color.

--- src/ui/control/EmojiButton.js
+++ src/ui/control/EmojiButton.js
@@ -7,15 +7,11 @@ import { Twemoji } from "ui/control";
 
 const EmojiButton = ({icon, emoji, caption, color, invisible, className, buttonRef, onMouseEnter, onMouseLeave,
                       onClick}) => (
-    <button className={cx(className, {"invisible": invisible})} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}
-            onClick={onClick} ref={buttonRef}>
+    <button className={cx(className, {"invisible": invisible})}  style={color ? {color} : null}
+            onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick} ref={buttonRef}>
         {icon && <FontAwesomeIcon icon={icon}/>}
         {emoji && <Twemoji code={emoji}/>}
-        {color ?
-            <span className="caption" style={{color}}>{caption}</span>
-        :
-            <span className="caption">{caption}</span>
-        }
+        <span className="caption">{caption}</span>
     </button>
 );
 
