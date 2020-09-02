ad725ed13ba9a17296a0a672450af2acc9b94575
Eliyahu Duvidzon <eliduvid1@gmail.com>
2020-09-02 21:30:17 +0300

    Make one-emoji bodies big

--- src/util/html.js
+++ src/util/html.js
@@ -1,18 +1,38 @@
 import sanitizeHtml from 'sanitize-html';
 import { parse as parseEmojis } from 'twemoji-parser';
 
+function emojiToImgTag(emoji, size=1) {
+    return `<img src="${emoji.url}" alt="${emoji.text}" style="width: ${size}em; height: ${size}em">`;
+}
+
+const MAX_LENGTH_TO_TRY_PARSE_AS_SINGLE_EMOJI = 50;
+const HTML_WITH_ONLY_EMOJI_BEFORE_REGEX = /^(\s*<\s*(mr-spoiler(\s+title="[^"]*")?|p)\s*>\s*)*/;
+const HTML_WITH_ONLY_EMOJI_AFTER_REGEX = /^(\s*<\/(mr-spoiler|p)>\s*)*$/;
+
 export function replaceEmojis(html) {
     if (!html) {
         return "";
     }
 
-    const entities = parseEmojis(html);
+    const emojis = parseEmojis(html);
     let shift = 0;
     let current = html;
-    for (let entity of entities) {
-        const img = `<img src="${entity.url}" alt="${entity.text}" style="width: 1em; height: 1em">`;
-        current = current.substring(0, entity.indices[0] + shift) + img + current.substring(entity.indices[1] + shift);
-        shift += img.length - (entity.indices[1] - entity.indices[0]);
+    //debugger;
+    if (emojis.length === 1 && html.length <= MAX_LENGTH_TO_TRY_PARSE_AS_SINGLE_EMOJI) {
+        //debugger;
+        const emoji = emojis[0];
+        const beforeEmoji = html.substring(0, emoji.indices[0]);
+        const afterEmoji = html.substring(emoji.indices[1]);
+        if (HTML_WITH_ONLY_EMOJI_BEFORE_REGEX.test(beforeEmoji) &&
+            HTML_WITH_ONLY_EMOJI_AFTER_REGEX.test(afterEmoji)) {
+            return beforeEmoji + emojiToImgTag(emoji, 3) + afterEmoji;
+        }
+    }
+
+    for (let emoji of emojis) {
+        const img = emojiToImgTag(emoji);
+        current = current.substring(0, emoji.indices[0] + shift) + img + current.substring(emoji.indices[1] + shift);
+        shift += img.length - (emoji.indices[1] - emoji.indices[0]);
     }
 
     return current;
