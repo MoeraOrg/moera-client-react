a520b42f832ee0651cf78ebcfdac0c8e1eade3fe
Eliyahu Duvidzon <eliduvid1@gmail.com>
2020-09-06 02:34:37 +0300

    Removed unnecessary braces

--- src/ui/comment/Comment.js
+++ src/ui/comment/Comment.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import cx from 'classnames';
 
-import { isConnectedToHome } from "state/home/selectors";
-import { isPermitted } from "state/node/selectors";
+import {isConnectedToHome} from "state/home/selectors";
+import {isPermitted} from "state/node/selectors";
 import EntryHtml from "ui/posting/EntryHtml";
 import CommentMenu from "ui/comment/CommentMenu";
 import CommentOwner from "ui/comment/CommentOwner";
@@ -12,7 +12,7 @@ import CommentUpdated from "ui/comment/CommentUpdated";
 import CommentDeleting from "ui/comment/CommentDeleting";
 import CommentButtons from "ui/comment/CommentButtons";
 import CommentReactions from "ui/comment/CommentReactions";
-import { getCommentsState } from "state/detailedposting/selectors";
+import {getCommentsState} from "state/detailedposting/selectors";
 import "./Comment.css";
 
 class Content extends React.PureComponent {
@@ -58,29 +58,28 @@ class Content extends React.PureComponent {
 
 }
 
-const Comment = ({postingId, comment, focused, isPermitted, connectedToHome}) =>
-    (
-        <div className={cx("comment", "entry", {"focused": focused, "single-emoji": comment.singleEmoji})}
-             data-moment={comment.moment}>
-            {comment.deleting ?
-                <CommentDeleting/>
-                :
-                <>
-                    <CommentMenu comment={comment} postingId={postingId} isPermitted={isPermitted}/>
-                    <div className="owner-line">
-                        <CommentOwner comment={comment}/>
-                        <CommentDate postingId={postingId} comment={comment}/>
-                        <CommentUpdated comment={comment}/>
-                    </div>
-                    <Content className="content" comment={comment}/>
-                    <div className="reactions-line">
-                        {connectedToHome && <CommentButtons comment={comment}/>}
-                        <CommentReactions postingId={postingId} comment={comment}/>
-                    </div>
-                </>
-            }
-        </div>
-    );
+const Comment = ({postingId, comment, focused, isPermitted, connectedToHome}) => (
+    <div className={cx("comment", "entry", {"focused": focused, "single-emoji": comment.singleEmoji})}
+         data-moment={comment.moment}>
+        {comment.deleting ?
+            <CommentDeleting/>
+            :
+            <>
+                <CommentMenu comment={comment} postingId={postingId} isPermitted={isPermitted}/>
+                <div className="owner-line">
+                    <CommentOwner comment={comment}/>
+                    <CommentDate postingId={postingId} comment={comment}/>
+                    <CommentUpdated comment={comment}/>
+                </div>
+                <Content className="content" comment={comment}/>
+                <div className="reactions-line">
+                    {connectedToHome && <CommentButtons comment={comment}/>}
+                    <CommentReactions postingId={postingId} comment={comment}/>
+                </div>
+            </>
+        }
+    </div>
+);
 
 export default connect(
     state => ({
