881dd1820d9c03a6b5f0da44eadae8b50916ab28
Eliyahu Duvidzon <57770205+eliduvid@users.noreply.github.com>
2020-09-23 16:53:52 +0300

    Enforcing text/plain MIME-type for original page
    Enforcing text/plain MIME-type for original page (the page generated by the node and replaced by the client). This way the original page can't inject any malicious scripts.
    
    Authored-by: Eliyahu Duvidzon <eliduvid1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -52,19 +52,24 @@ function buildInitAction() {
     return initFromLocation(rootLocation, path, query, hash);
 }
 
-initIconLibrary();
-simpleNumberLocalizer();
-dateFnsLocalizer();
-registerSpoilerElement();
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("app-root")
-);
-store.dispatch(buildInitAction());
+if (document.contentType === 'text/plain') {
+    initIconLibrary();
+    simpleNumberLocalizer();
+    dateFnsLocalizer();
+    registerSpoilerElement();
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById("app-root")
+    );
+    store.dispatch(buildInitAction());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+} else {
+    document.body.innerText = `Pages with content type '${document.contentType}' are not supported anymore for security reasons.
+    Please turn to administrator to upgrade the node software to version 0.9.0 or later.`
+}
