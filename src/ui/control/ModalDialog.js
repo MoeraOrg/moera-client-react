79d2011c3ba8785e20e52466187b9e239403a13f
Shmuel Melamud <smelamud@redhat.com>
2020-11-19 02:35:41 +0200

    Do now allow modals to be higher than screen. Add vertical scrolling, if does not fit.

--- src/ui/control/ModalDialog.js
+++ src/ui/control/ModalDialog.js
@@ -3,40 +3,59 @@ import PropType from 'prop-types';
 import * as ReactDOM from 'react-dom';
 import cx from 'classnames';
 
-export const ModalDialog = ({title, size, children, onClose}) => {
-    return ReactDOM.createPortal(
-        <>
-            <div className="modal-backdrop show" />
-            <div className="modal show" style={{display: "block"}} onClick={onClose}
-                 onKeyDown={e => {
-                     if (e.key === "Escape" && onClose) {
-                         onClose();
-                     }
-                 }}>
-                <div className={cx(
-                            "modal-dialog",
-                            "modal-dialog-centered", {
-                                [`modal-${size}`]: !!size
-                            }
-                        )} onClick={e => e.stopPropagation()}>
-                    <div className="modal-content">{
-                        title &&
-                            <div className="modal-header">
-                                <h4 className="modal-title">{title}</h4>
-                                {onClose && <button type="button" className="close" onClick={onClose}>&times;</button>}
-                            </div>
+import "./ModalDialog.css";
+
+export class ModalDialog extends React.PureComponent {
+
+    static propTypes = {
+        title: PropType.string,
+        size: PropType.string,
+        onClose: PropType.func
+    };
+
+    onModalKeyDown = e => {
+        const {onClose} = this.props;
+
+        if (e.key === "Escape" && onClose) {
+            onClose();
+        }
+    }
+
+    onModalDialogClick = e => e.stopPropagation();
+
+    render() {
+        const {title, size, children, onClose} = this.props;
+
+        return ReactDOM.createPortal(
+            <>
+                <div className="modal-backdrop show"/>
+                <div className="modal show" onClick={onClose} onKeyDown={this.onModalKeyDown}>
+                    <div className={cx(
+                        "modal-dialog",
+                        "modal-dialog-centered",
+                        {
+                            [`modal-${size}`]: !!size
                         }
-                        {children}
+                    )} onClick={this.onModalDialogClick}>
+                        <div className="modal-content">
+                            {title &&
+                                <div className="modal-header">
+                                    <h4 className="modal-title">{title}</h4>
+                                    {onClose &&
+                                        <button type="button" className="close" onClick={onClose}>&times;</button>
+                                    }
+                                </div>
+                            }
+                            <div className="modal-children">
+                                {children}
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>,
-        document.getElementById("modal-root")
-    );
-};
+            </>,
+            document.getElementById("modal-root")
+        );
+    }
 
-ModalDialog.propTypes = {
-    title: PropType.string,
-    size: PropType.string,
-    onClose: PropType.func
-};
+}
+
