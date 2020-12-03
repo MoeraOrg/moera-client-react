094ce543b6878829ab83f237d4fc3633a967e247
Shmuel Melamud <smelamud@redhat.com>
2020-12-04 00:49:27 +0200

    Display small "home" icon in front of the node name, if user is at his home node. Highlight Home button in the top-right corner only if he is at his Timeline.

--- src/ui/mainmenu/connectionstatus/HomeButton.js
+++ src/ui/mainmenu/connectionstatus/HomeButton.js
@@ -4,18 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cx from 'classnames';
 
 import { isAtHomeNode } from "state/node/selectors";
+import { isAtTimelinePage } from "state/navigation/selectors";
 import Jump from "ui/navigation/Jump";
 import "./HomeButton.css";
 
-const HomeButton = ({atHome}) => (
-    <Jump nodeName=":" href="/" className={cx("connection-button", "home-button", {"active": atHome})}
-          title={atHome ? "Home node" : "You are at your home node"}>
+const HomeButton = ({atHomeTimeline}) => (
+    <Jump nodeName=":" href="/" className={cx("connection-button", "home-button", {"active": atHomeTimeline})}
+          title={"Your timeline"}>
         <FontAwesomeIcon icon="home"/>
     </Jump>
 );
 
 export default connect(
     state => ({
-        atHome: isAtHomeNode(state),
+        atHomeTimeline: isAtHomeNode(state) && isAtTimelinePage(state),
     })
 )(HomeButton);
--- src/ui/mainmenu/owner/OwnerName.js
+++ src/ui/mainmenu/owner/OwnerName.js
@@ -3,13 +3,14 @@ import PropType from 'prop-types';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { isNodeAdmin } from "state/node/selectors";
+import { isAtHomeNode, isNodeAdmin } from "state/node/selectors";
 import { isOwnerNameExpiring } from "state/owner/selectors";
 import { NodeName } from "ui/control";
 import "./OwnerName.css";
 
-const OwnerName = ({onClick, name, expiring, ...props}) => (
+const OwnerName = ({onClick, name, expiring, atHome, ...props}) => (
     <span id="owner" className="navbar-text" onClick={onClick}>
+        {atHome && <span className="home" title="You are at your home node"><FontAwesomeIcon icon="home"/></span>}
         <span id="owner-name">{name ? <NodeName name={name} linked={false} {...props}/> : "no name set"}</span>
         {expiring &&
             <span className="owner-name-expiring" title="Need to prolong the name">
@@ -30,6 +31,7 @@ OwnerName.propTypes = {
 export default connect(
     state => ({
         ...state.owner,
-        expiring: isNodeAdmin(state) && isOwnerNameExpiring(state)
+        expiring: isNodeAdmin(state) && isOwnerNameExpiring(state),
+        atHome: isAtHomeNode(state)
     })
 )(OwnerName);
