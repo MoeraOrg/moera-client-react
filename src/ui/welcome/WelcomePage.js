721c699385906354d9dc243efecd7fcc391357d4
Shmuel Melamud <smelamud@redhat.com>
2021-01-11 22:24:27 +0200

    Link to the catalog of blogs on the Welcome page.

--- src/ui/welcome/WelcomePage.js
+++ src/ui/welcome/WelcomePage.js
@@ -9,6 +9,10 @@ const WelcomePage = () => (
     <Page className="welcome">
         <div className="title">Welcome!</div>
         <WelcomeNavigator/>
+        <div className="discover">
+            <a className="btn btn-primary btn-sm"
+               href="https://lamed.moera.blog/moera/post/1549a6ef-2ea8-47ce-9643-abebc95e3d74">Find more people &rarr;</a>
+        </div>
         <Invitation/>
     </Page>
 );
