a9ddaf2f9b75cb02b5c8cb43e19b3ff062967f8c
Shmuel Melamud <smelamud@redhat.com>
2021-01-27 01:59:18 +0200

    Do not show word "comments" in comments counter on small screens.

--- src/ui/posting/PostingComments.js
+++ src/ui/posting/PostingComments.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Jump from "ui/navigation/Jump";
+import { Browser } from "ui/browser";
 import "./PostingComments.css";
 
-const PostingComments = ({posting}) => (
-    posting.totalComments > 0 ?
+const PostingComments = ({posting}) => {
+    if (posting.totalComments <= 0) {
+        return null;
+    }
+
+    let commentsText = "";
+    if (!Browser.isTinyScreen()) {
+        commentsText = posting.totalComments === 1 ? "comment" : "comments"
+    }
+    return (
         <div className="posting-comments">
             <Jump className="total-comments" href={`/post/${posting.id}#comments`}>
                 <FontAwesomeIcon icon="comment"/>{" "}&nbsp;
-                {posting.totalComments}&nbsp;{posting.totalComments === 1 ? "comment" : "comments"}
+                {posting.totalComments}&nbsp;{commentsText}
             </Jump>
         </div>
-    :
-        null
-);
+    );
+}
 
 export default PostingComments;
