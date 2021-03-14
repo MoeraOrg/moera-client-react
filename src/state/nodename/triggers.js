2e2776d5fbd6b490d75ccdfbda6452c4f8e3a8b4
Shmuel Melamud <smelamud@redhat.com>
2021-03-14 03:12:19 +0200

    Flashbox about starting a naming operation.

--- src/state/nodename/triggers.js
+++ src/state/nodename/triggers.js
@@ -11,6 +11,7 @@ import {
 } from "state/nodename/actions";
 import { EVENT_NODE_NODE_NAME_CHANGED, EVENT_NODE_REGISTERED_NAME_OPERATION_STATUS } from "api/events/actions";
 import { isAtNode } from "state/node/selectors";
+import { flashBox } from "state/flashbox/actions";
 
 export default [
     trigger(GO_TO_PAGE, conj(isAtProfilePage, isNodeNameToBeLoaded), nodeNameLoad),
@@ -18,6 +19,7 @@ export default [
     trigger([CONNECTED_TO_HOME, DISCONNECTED_FROM_HOME], inv(isAtProfilePage), nodeNameUnset),
     trigger(REGISTER_NAME_SUCCEEDED, isAtNode, nodeNameLoad),
     trigger(NODE_NAME_UPDATE_SUCCEEDED, true, nodeNameLoad),
+    trigger(NODE_NAME_UPDATE_SUCCEEDED, true, flashBox("Name operation started")),
     trigger(
         [EVENT_NODE_REGISTERED_NAME_OPERATION_STATUS, EVENT_NODE_NODE_NAME_CHANGED],
         isAtProfilePage,
