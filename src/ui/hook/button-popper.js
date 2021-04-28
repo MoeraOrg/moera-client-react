c05f8c219e5fefecdad5cc6e8281ddc380794211
Shmuel Melamud <smelamud@redhat.com>
2021-04-28 22:57:25 +0300

    Rewrite VerticalMenuToggler with useButtonPopper() hook.

--- src/ui/hook/button-popper.js
+++ src/ui/hook/button-popper.js
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { usePopper } from 'react-popper';
 
-export function useButtonPopper(placement) {
+export function useButtonPopper(placement, modifiers = null) {
     const [visible, setVisible] = useState(false);
 
     const onToggle = useCallback(event => {
@@ -17,15 +17,17 @@ export function useButtonPopper(placement) {
     useEffect(() => {
         if (visible) {
             document.getElementById("app-root").addEventListener("click", onHide);
+            document.getElementById("modal-root").addEventListener("click", onHide);
             return () => {
                 document.getElementById("app-root").removeEventListener("click", onHide);
+                document.getElementById("modal-root").removeEventListener("click", onHide);
             }
         }
     }, [visible, onHide])
 
     const [buttonRef, setButtonRef] = useState(null);
     const [popperRef, setPopperRef] = useState(null);
-    const {styles, attributes} = usePopper(buttonRef, popperRef, {placement});
+    const {styles, attributes} = usePopper(buttonRef, popperRef, {placement, modifiers});
 
     return {
         visible, onToggle, setButtonRef, setPopperRef,
--- src/ui/mainmenu/vertical/VerticalMenuToggler.js
+++ src/ui/mainmenu/vertical/VerticalMenuToggler.js
@@ -1,72 +1,28 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Manager, Popper, Reference } from 'react-popper';
 
+import { useButtonPopper } from "ui/hook";
 import VerticalMenu from "ui/mainmenu/vertical/VerticalMenu";
 import "./VerticalMenuToggler.css";
 
-class VerticalMenuToggler extends React.PureComponent {
-    constructor(props, context) {
-        super(props, context);
+export default function VerticalMenuToggler() {
+    const {
+        visible, onToggle, setButtonRef, setPopperRef, popperStyles, popperAttributes
+    } = useButtonPopper("bottom", {strategy: "fixed"});
 
-        this.state = {visible: false};
-    }
-
-    toggle = () => {
-        if (!this.state.visible) {
-            this.show();
-        } else {
-            this.hide();
-        }
-    };
-
-    documentClick = () => {
-        this.hide();
-    };
-
-    show = () => {
-        if (this.state.visible) {
-            return;
-        }
-        this.setState({visible: true});
-        document.getElementById("app-root").addEventListener("click", this.documentClick);
-    };
-
-    hide = () => {
-        if (!this.state.visible) {
-            return;
-        }
-        this.setState({visible: false});
-        document.getElementById("app-root").removeEventListener("click", this.documentClick);
-    };
-
-    render() {
-        const {visible} = this.state;
-
-        return (
-            <Manager>
-                <Reference>
-                    {({ref}) => (
-                        <button ref={ref} className="navbar-toggler" type="button" onClick={this.toggle}>
-                            <span className="navbar-toggler-icon"/>
-                        </button>
-                    )}
-                </Reference>
-                {ReactDOM.createPortal(
-                    visible &&
-                        <Popper placement="bottom" strategy="fixed">
-                            {({ref, style}) => (
-                                <div ref={ref} style={style} className="vertical-menu-popper">
-                                    <VerticalMenu/>
-                                </div>
-                            )}
-                        </Popper>,
+    return (
+        <>
+            <button ref={setButtonRef} className="navbar-toggler" type="button" onClick={onToggle}>
+                <span className="navbar-toggler-icon"/>
+            </button>
+            {visible &&
+                ReactDOM.createPortal(
+                    <div ref={setPopperRef} style={popperStyles} {...popperAttributes} className="vertical-menu-popper">
+                        <VerticalMenu/>
+                    </div>,
                     document.querySelector("#modal-root")
-                )}
-            </Manager>
-        );
-    }
-
+                )
+            }
+        </>
+    );
 }
-
-export default VerticalMenuToggler;
