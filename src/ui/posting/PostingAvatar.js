392af11d0f5e6fc7d8d1008ad26be674cbedbd73
Shmuel Melamud <smelamud@redhat.com>
2021-05-09 01:45:20 +0300

    Show the node card when hovering an avatar.

--- src/ui/posting/PostingAvatar.js
+++ src/ui/posting/PostingAvatar.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import Jump from "ui/navigation/Jump";
 import { Avatar } from "ui/control";
+import NodeNamePopup from "ui/nodename/NodeNamePopup";
 
 const PostingAvatar = ({posting}) => (
-    <Jump nodeName={posting.ownerName} href="/profile" title="Profile">
-        <Avatar avatar={posting.ownerAvatar} size={48}/>
-    </Jump>
+    <NodeNamePopup nodeName={posting.ownerName} fullName={posting.ownerFullName}>
+        {(ref, mainEnter, mainLeave, mainTouch) =>
+            <Jump nodeName={posting.ownerName} href="/profile" anchorRef={ref} onMouseEnter={mainEnter}
+                  onMouseLeave={mainLeave} onTouchStart={mainTouch}>
+                <Avatar avatar={posting.ownerAvatar} size={48}/>
+            </Jump>
+        }
+    </NodeNamePopup>
 );
 
 export default PostingAvatar;
