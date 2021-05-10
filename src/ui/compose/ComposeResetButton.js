ca3ce4b970e7e539493eff78c16d798f29910a93
Shmuel Melamud <smelamud@redhat.com>
2021-05-10 04:37:07 +0300

    ComposeResetButton migrated to hooks.

--- src/ui/compose/ComposeResetButton.js
+++ src/ui/compose/ComposeResetButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -7,11 +7,8 @@ import { composeDraftListItemDelete, composeDraftRevisionDelete } from "state/co
 import { confirmBox } from "state/confirmbox/actions";
 import "./ComposeResetButton.css";
 
-class ComposeResetButton extends React.PureComponent {
-
-    onClick = () => {
-        const {postingId, draftId, confirmBox} = this.props;
-
+function ComposeResetButton({postingId, draftId, posting, confirmBox}) {
+    const onClick = useCallback(() => {
         if (postingId == null) {
             confirmBox("Do you really want to delete the draft?", "Yes", "No",
                 composeDraftListItemDelete(draftId));
@@ -19,33 +16,28 @@ class ComposeResetButton extends React.PureComponent {
             confirmBox("Do you really want to forget all changes?", "Yes", "No",
                 composeDraftRevisionDelete());
         }
-    };
+    }, [postingId, draftId, confirmBox]);
 
-    render() {
-        const {postingId, draftId, posting} = this.props;
-
-        if (postingId == null) {
-            if (draftId == null) {
-                return null;
-            }
-            return (
-                <Button variant="danger" className="reset-button" title="Delete draft" onClick={this.onClick}>
-                    <FontAwesomeIcon icon="trash-alt"/>
-                </Button>
-            );
-        } else {
-            if (posting == null || !posting.draftPending) {
-                return null;
-            }
-            return (
-                <Button variant="info" className="reset-button" onClick={this.onClick}>
-                    <FontAwesomeIcon icon="undo-alt"/>
-                    {" "}Undo
-                </Button>
-            );
+    if (postingId == null) {
+        if (draftId == null) {
+            return null;
         }
+        return (
+            <Button variant="danger" className="reset-button" title="Delete draft" onClick={onClick}>
+                <FontAwesomeIcon icon="trash-alt"/>
+            </Button>
+        );
+    } else {
+        if (posting == null || !posting.draftPending) {
+            return null;
+        }
+        return (
+            <Button variant="info" className="reset-button" onClick={this.onClick}>
+                <FontAwesomeIcon icon="undo-alt"/>
+                {" "}Undo
+            </Button>
+        );
     }
-
 }
 
 export default connect(
