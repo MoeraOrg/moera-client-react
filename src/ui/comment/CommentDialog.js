586d26c29525d98d39d030d03b47a69885a08f3f
Shmuel Melamud <smelamud@redhat.com>
2021-05-18 03:01:09 +0300

    Migrated CommentDialog to hooks.

--- src/ui/comment/CommentDialog.js
+++ src/ui/comment/CommentDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Form, withFormik } from 'formik';
 import * as textFieldEdit from 'text-field-edit';
@@ -11,24 +11,22 @@ import { Browser } from "ui/browser";
 import { Button, ConflictWarning, ModalDialog } from "ui/control";
 import { RichTextField } from "ui/control/field";
 import commentComposeLogic from "ui/comment/comment-compose-logic";
-import { replaceSmileys } from "util/text";
 import { parseBool } from "util/misc";
 
-class CommentDialog extends React.PureComponent {
+function CommentDialog(props) {
+    const {
+        comment, show, conflict, beingPosted, smileysEnabled, sourceFormatDefault, closeCommentDialog,
+        commentDialogConflictClose, submitKey, submitForm, resetForm
+    } = props;
+    const commentId = comment != null ? comment.id : null;
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if ((this.props.show && !prevProps.show)
-            || (this.props.comment != null && prevProps.comment == null)
-            || (this.props.comment != null && this.props.comment.id !== prevProps.comment.id)) {
+    useEffect(() => {
+        const values = commentComposeLogic.mapPropsToValues(props);
+        resetForm({values});
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [show, commentId]); // 'props' are missing on purpose
 
-            const values = commentComposeLogic.mapPropsToValues(this.props);
-            this.props.resetForm({values});
-        }
-    }
-
-    onKeyDown = (event) => {
-        const {submitKey, submitForm} = this.props;
-
+    const onKeyDown = event => {
         if (event.key === "Enter") {
             const submit = !Browser.isTouchScreen() && !event.shiftKey
                 && ((submitKey === "enter" && !event.ctrlKey) || (submitKey === "ctrl-enter" && event.ctrlKey));
@@ -41,41 +39,27 @@ class CommentDialog extends React.PureComponent {
         }
     }
 
-    onInput = event => {
-        if (this.props.smileysEnabled && (event.inputType === "insertLineBreak"
-            || (event.inputType.startsWith("insert") && event.data != null && event.data.match(/\s/)))) {
-
-            event.target.value = replaceSmileys(event.target.value, false);
-        }
+    if (!show) {
+        return null;
     }
 
-    render() {
-        const {show, conflict, beingPosted, smileysEnabled, sourceFormatDefault, closeCommentDialog,
-               commentDialogConflictClose} = this.props;
-
-        if (!show) {
-            return null;
-        }
-
-        return (
-            <ModalDialog title="Edit a comment" onClose={closeCommentDialog}>
-                <ConflictWarning text="The comment was edited by somebody." show={conflict}
-                                 onClose={commentDialogConflictClose}/>
-                <Form>
-                    <div className="modal-body">
-                        <RichTextField name="body" rows={5} anyValue autoFocus disabled={beingPosted}
-                                       smileysEnabled={smileysEnabled} format={sourceFormatDefault}
-                                       onKeyDown={this.onKeyDown}/>
-                    </div>
-                    <div className="modal-footer">
-                        <Button variant="secondary" onClick={closeCommentDialog}>Cancel</Button>
-                        <Button variant="primary" type="submit" loading={beingPosted}>Update</Button>
-                    </div>
-                </Form>
-            </ModalDialog>
-        );
-    }
-
+    return (
+        <ModalDialog title="Edit a comment" onClose={closeCommentDialog}>
+            <ConflictWarning text="The comment was edited by somebody." show={conflict}
+                             onClose={commentDialogConflictClose}/>
+            <Form>
+                <div className="modal-body">
+                    <RichTextField name="body" rows={5} anyValue autoFocus disabled={beingPosted}
+                                   smileysEnabled={smileysEnabled} format={sourceFormatDefault}
+                                   onKeyDown={onKeyDown}/>
+                </div>
+                <div className="modal-footer">
+                    <Button variant="secondary" onClick={closeCommentDialog}>Cancel</Button>
+                    <Button variant="primary" type="submit" loading={beingPosted}>Update</Button>
+                </div>
+            </Form>
+        </ModalDialog>
+    );
 }
 
 export default connect(
