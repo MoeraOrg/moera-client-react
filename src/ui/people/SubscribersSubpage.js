4160064c8a82835883627c6b99a9577dbbc25813
Shmuel Melamud <smelamud@redhat.com>
2021-05-20 22:58:57 +0300

    Display avatars in People page.

--- src/ui/people/SubscribersSubpage.js
+++ src/ui/people/SubscribersSubpage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Loading } from "ui/control";
+import { AvatarWithPopup, Loading } from "ui/control";
 import NodeName from "ui/nodename/NodeName";
 
 const SubscribersSubpage = ({loading, subscribers}) => (
     <div className="row">
         <Loading active={loading}/>
         {subscribers.map(sr =>
-            <div key={sr.id} className="col-md-3 col-sm-4">
+            <div key={sr.id} className="person col-sm-4">
+                <AvatarWithPopup ownerName={sr.nodeName} ownerFullName={sr.fullName} avatar={sr.avatar} size={48}/>
                 <NodeName name={sr.nodeName} fullName={sr.fullName}/>
             </div>
         )}
--- src/ui/people/SubscriptionsSubpage.js
+++ src/ui/people/SubscriptionsSubpage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Loading } from "ui/control";
+import { AvatarWithPopup, Loading } from "ui/control";
 import NodeName from "ui/nodename/NodeName";
 
 const SubscriptionsSubpage = ({loading, subscriptions}) => (
     <div className="row">
         <Loading active={loading}/>
         {subscriptions.map(sr =>
-            <div key={sr.id} className="subscription col-md-3 col-sm-4">
+            <div key={sr.id} className="person col-sm-4">
+                <AvatarWithPopup ownerName={sr.nodeName} ownerFullName={sr.fullName} avatar={sr.avatar} size={48}/>
                 <NodeName name={sr.remoteNodeName} fullName={sr.remoteFullName}/>
             </div>
         )}
