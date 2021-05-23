ac1ca3e86e4ebe5cb0f46bb5ff3cdc03c959f2fc
Shmuel Melamud <smelamud@redhat.com>
2021-05-24 02:18:31 +0300

    InstantStory migrated to hooks.

--- src/ui/instant/InstantStory.js
+++ src/ui/instant/InstantStory.js
@@ -12,11 +12,8 @@ import { getInstantTarget } from "ui/instant/instant-types";
 import InstantHtml from "ui/instant/InstantHtml";
 import "./InstantStory.css";
 
-class InstantStory extends React.PureComponent {
-
-    onJump = story => (href, performJump) => {
-        const {hide, storyReadingUpdate} = this.props;
-
+function InstantStory({story, lastNew, hide, storyReadingUpdate}) {
+    const onJump = (href, performJump) => {
         hide();
         performJump();
         if (!story.read) {
@@ -24,36 +21,30 @@ class InstantStory extends React.PureComponent {
         }
     }
 
-    onEnvelope = () => {
-        const {story, storyReadingUpdate} = this.props;
-        storyReadingUpdate(":instant", story.id, !story.read);
-    }
+    const onEnvelope = () => storyReadingUpdate(":instant", story.id, !story.read);
 
-    render() {
-        const {story, lastNew} = this.props;
-        const {nodeName, href} = getInstantTarget(story);
-        return (
-            <div className={cx("instant", {"unread": !story.read, "last-new": lastNew})}>
-                <div className="cursor">
-                    <Jump nodeName={nodeName} href={href} trackingId={story.read ? null : story.trackingId}
-                          onNear={this.onJump(story)} onFar={this.onJump(story)}>
-                        <InstantHtml html={story.summary}/>
-                        <div className="footer">
-                            <InstantIcon story={story}/>
-                            <span className="date">{formatDistanceToNow(fromUnixTime(story.publishedAt))}</span>
-                        </div>
-                    </Jump>
-                    <div className="sidebar">
-                        <span className="envelope" title={story.read ? "Mark as Unread" : "Mark as Read"}
-                              onClick={this.onEnvelope}>
-                            <FontAwesomeIcon icon={["far", story.read ? "envelope-open" : "envelope"]}/>
-                        </span>
+    const {nodeName, href} = getInstantTarget(story);
+
+    return (
+        <div className={cx("instant", {"unread": !story.read, "last-new": lastNew})}>
+            <div className="cursor">
+                <Jump nodeName={nodeName} href={href} trackingId={story.read ? null : story.trackingId}
+                      onNear={onJump} onFar={onJump}>
+                    <InstantHtml html={story.summary}/>
+                    <div className="footer">
+                        <InstantIcon story={story}/>
+                        <span className="date">{formatDistanceToNow(fromUnixTime(story.publishedAt))}</span>
                     </div>
+                </Jump>
+                <div className="sidebar">
+                    <span className="envelope" title={story.read ? "Mark as Unread" : "Mark as Read"}
+                          onClick={onEnvelope}>
+                        <FontAwesomeIcon icon={["far", story.read ? "envelope-open" : "envelope"]}/>
+                    </span>
                 </div>
             </div>
-        );
-    }
-
+        </div>
+    );
 }
 
 export default connect(
