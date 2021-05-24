335e2ff1c2df909f92b7e7c341083ac162b70cac
Shmuel Melamud <smelamud@redhat.com>
2021-05-24 15:45:49 +0300

    Added avatar to InstantStory.

--- src/ui/instant/InstantStory.js
+++ src/ui/instant/InstantStory.js
@@ -7,6 +7,7 @@ import { formatDistanceToNow, fromUnixTime } from 'date-fns';
 
 import InstantIcon from "ui/instant/InstantIcon";
 import Jump from "ui/navigation/Jump";
+import { Avatar } from "ui/control";
 import { storyReadingUpdate } from "state/stories/actions";
 import { getInstantTarget } from "ui/instant/instant-types";
 import InstantHtml from "ui/instant/InstantHtml";
@@ -24,24 +25,27 @@ function InstantStory({story, lastNew, hide, storyReadingUpdate}) {
     const onEnvelope = () => storyReadingUpdate(":instant", story.id, !story.read);
 
     const {nodeName, href} = getInstantTarget(story);
+    const trackingId = story.read ? null : story.trackingId;
 
     return (
         <div className={cx("instant", {"unread": !story.read, "last-new": lastNew})}>
-            <div className="cursor">
-                <Jump nodeName={nodeName} href={href} trackingId={story.read ? null : story.trackingId}
-                      onNear={onJump} onFar={onJump}>
-                    <InstantHtml html={story.summary}/>
-                    <div className="footer">
-                        <InstantIcon story={story}/>
-                        <span className="date">{formatDistanceToNow(fromUnixTime(story.publishedAt))}</span>
-                    </div>
-                </Jump>
-                <div className="sidebar">
-                    <span className="envelope" title={story.read ? "Mark as Unread" : "Mark as Read"}
-                          onClick={onEnvelope}>
-                        <FontAwesomeIcon icon={["far", story.read ? "envelope-open" : "envelope"]}/>
-                    </span>
-                </div>
+            <Jump nodeName={nodeName} href={href} trackingId={trackingId} className="summary-avatar"
+                  onNear={onJump} onFar={onJump}>
+                <Avatar avatar={story.summaryAvatar} nodeName=":" size={36}/>
+            </Jump>
+            <Jump nodeName={nodeName} href={href} trackingId={trackingId} className="summary"
+                  onNear={onJump} onFar={onJump}>
+                <InstantHtml html={story.summary}/>
+            </Jump>
+            <div className="footer">
+                <InstantIcon story={story}/>
+                <span className="date">{formatDistanceToNow(fromUnixTime(story.publishedAt))}</span>
+            </div>
+            <div className="sidebar">
+                <span className="envelope" title={story.read ? "Mark as Unread" : "Mark as Read"}
+                      onClick={onEnvelope}>
+                    <FontAwesomeIcon icon={["far", story.read ? "envelope-open" : "envelope"]}/>
+                </span>
             </div>
         </div>
     );
