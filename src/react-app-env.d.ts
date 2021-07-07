1c5161db91979d1d830a493aece961b7a17385dd
Shmuel Melamud <smelamud@redhat.com>
2021-07-08 02:10:09 +0300

    Added Android JS interface definition.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+interface AndroidJsInterface {
+    locationChanged(url: string, location: string): void;
+    connectedToHome(url: string, token: string, ownerName: string): void;
+    loadSettingsMeta(): string;
+    loadSettings(): string;
+    storeSettings(data: string): void;
+    share(url: string, title: string): void;
+    getSharedText(): string;
+    getSharedTextType(): string;
+    back(): void;
+    log(text: string): void;
+}
+
+interface Window {
+    Android?: AndroidJsInterface;
+}
