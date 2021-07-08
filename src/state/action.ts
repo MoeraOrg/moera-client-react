de9c4009bb79f25a05d35193bdaaabe188d55b42
Shmuel Melamud <smelamud@redhat.com>
2021-07-08 04:42:35 +0300

    Converted confirmbox/actions to TS.

--- src/state/action.ts
+++ src/state/action.ts
@@ -1,8 +1,10 @@
 import { CartesAction } from "state/cartes/actions";
 import { ChangeDateDialogAction } from "state/changedatedialog/actions";
 import { ComposeAction } from "state/compose/actions";
+import { ConfirmBoxAnyAction } from "state/confirmbox/actions";
 
 export type ClientAction =
     CartesAction
     | ChangeDateDialogAction
-    | ComposeAction;
+    | ComposeAction
+    | ConfirmBoxAnyAction;
---
+++ src/state/confirmbox/actions.ts
@@ -0,0 +1,26 @@
+import { ActionBase } from "state/action-base";
+
+export const CONFIRM_BOX = "CONFIRM_BOX";
+type ConfirmBoxAction = ActionBase<typeof CONFIRM_BOX, {
+    message: string;
+    yes: string;
+    no: string;
+    onYes: any;
+    onNo: any;
+    variant: string;
+}>;
+export const confirmBox = (message: string, yes: string = "Yes", no: string = "No", onYes: any = null, onNo: any = null,
+                           variant: string = "primary"): ConfirmBoxAction => ({
+    type: CONFIRM_BOX,
+    payload: {message, yes, no, onYes, onNo, variant}
+});
+
+export const CLOSE_CONFIRM_BOX = "CLOSE_CONFIRM_BOX";
+type CloseConfirmBoxAction = ActionBase<typeof CLOSE_CONFIRM_BOX, never>;
+export const closeConfirmBox = (): CloseConfirmBoxAction => ({
+    type: CLOSE_CONFIRM_BOX
+});
+
+export type ConfirmBoxAnyAction =
+    ConfirmBoxAction
+    | CloseConfirmBoxAction;
