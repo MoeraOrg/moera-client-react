8b9954da36c8c5c5c54b28859111fcd884e12b7c
Shmuel Melamud <smelamud@redhat.com>
2021-07-12 18:01:06 +0300

    Simplified definition of EventAction<E>.

--- src/api/events/actions.ts
+++ src/api/events/actions.ts
@@ -1,6 +1,5 @@
 import * as immutable from 'object-path-immutable';
 import {
-    BaseEvent,
     ClientSettingsChangedEvent,
     CommentAddedEvent,
     CommentDeletedEvent,
@@ -95,58 +94,57 @@ export const EVENT_HOME_REMOTE_NODE_AVATAR_CHANGED = "EVENT_HOME_REMOTE_NODE_AVA
 type EventSource = "HOME" | "NODE" | "RECEIVER";
 type EventActionType<T extends string> = `EVENT_${EventSource}_${T}`;
 
-interface EventAction<T extends string, E extends BaseEvent<T>> {
-    type: EventActionType<T>;
+interface EventAction<E extends {type: string}> {
+    type: EventActionType<E["type"]>;
     payload: Omit<E, "type">;
 }
 
-export const eventAction = <T extends string, E extends BaseEvent<T>>
-    (event: E, source: EventSource): EventAction<T, E> => ({
-        type: `EVENT_${source}_${event.type}`,
-        payload: immutable.del(event, "type")
-    });
+export const eventAction = <E extends {type: string}>(event: E, source: EventSource): EventAction<E> => ({
+    type: `EVENT_${source}_${event.type}`,
+    payload: immutable.del(event, "type")
+});
 
 export type ClientEventAction =
-    EventAction<SubscribedEvent["type"], SubscribedEvent>
-    | EventAction<PingEvent["type"], PingEvent>
-    | EventAction<ProfileUpdatedEvent["type"], ProfileUpdatedEvent>
-    | EventAction<NodeSettingsChangedEvent["type"], NodeSettingsChangedEvent>
-    | EventAction<ClientSettingsChangedEvent["type"], ClientSettingsChangedEvent>
-    | EventAction<RegisteredNameOperationStatusEvent["type"], RegisteredNameOperationStatusEvent>
-    | EventAction<NodeNameChangedEvent["type"], NodeNameChangedEvent>
-    | EventAction<FeedStatusUpdatedEvent["type"], FeedStatusUpdatedEvent>
-    | EventAction<StoriesStatusUpdatedEvent["type"], StoriesStatusUpdatedEvent>
-    | EventAction<RemoteNodeFullNameChangedEvent["type"], RemoteNodeFullNameChangedEvent>
-    | EventAction<PeopleChangedEvent["type"], PeopleChangedEvent>
-    | EventAction<RemoteNodeAvatarChangedEvent["type"], RemoteNodeAvatarChangedEvent>
-    | EventAction<PostingAddedEvent["type"], PostingAddedEvent>
-    | EventAction<PostingUpdatedEvent["type"], PostingUpdatedEvent>
-    | EventAction<PostingDeletedEvent["type"], PostingDeletedEvent>
-    | EventAction<PostingRestoredEvent["type"], PostingRestoredEvent>
-    | EventAction<PostingReactionsChangedEvent["type"], PostingReactionsChangedEvent>
-    | EventAction<PostingCommentsChangedEvent["type"], PostingCommentsChangedEvent>
-    | EventAction<DraftAddedEvent["type"], DraftAddedEvent>
-    | EventAction<DraftUpdatedEvent["type"], DraftUpdatedEvent>
-    | EventAction<DraftDeletedEvent["type"], DraftDeletedEvent>
-    | EventAction<RemotePostingVerifiedEvent["type"], RemotePostingVerifiedEvent>
-    | EventAction<RemotePostingVerificationFailedEvent["type"], RemotePostingVerificationFailedEvent>
-    | EventAction<RemoteReactionAddedEvent["type"], RemoteReactionAddedEvent>
-    | EventAction<RemoteReactionDeletedEvent["type"], RemoteReactionDeletedEvent>
-    | EventAction<RemoteReactionVerifiedEvent["type"], RemoteReactionVerifiedEvent>
-    | EventAction<RemoteReactionVerificationFailedEvent["type"], RemoteReactionVerificationFailedEvent>
-    | EventAction<StoryAddedEvent["type"], StoryAddedEvent>
-    | EventAction<StoryDeletedEvent["type"], StoryDeletedEvent>
-    | EventAction<StoryUpdatedEvent["type"], StoryUpdatedEvent>
-    | EventAction<SubscriberAddedEvent["type"], SubscriberAddedEvent>
-    | EventAction<SubscriberDeletedEvent["type"], SubscriberDeletedEvent>
-    | EventAction<SubscriptionAddedEvent["type"], SubscriptionAddedEvent>
-    | EventAction<SubscriptionDeletedEvent["type"], SubscriptionDeletedEvent>
-    | EventAction<CommentAddedEvent["type"], CommentAddedEvent>
-    | EventAction<CommentUpdatedEvent["type"], CommentUpdatedEvent>
-    | EventAction<CommentDeletedEvent["type"], CommentDeletedEvent>
-    | EventAction<CommentReactionsChangedEvent["type"], CommentReactionsChangedEvent>
-    | EventAction<RemoteCommentAddedEvent["type"], RemoteCommentAddedEvent>
-    | EventAction<RemoteCommentUpdatedEvent["type"], RemoteCommentUpdatedEvent>
-    | EventAction<RemoteCommentDeletedEvent["type"], RemoteCommentDeletedEvent>
-    | EventAction<RemoteCommentVerifiedEvent["type"], RemoteCommentVerifiedEvent>
-    | EventAction<RemoteCommentVerificationFailedEvent["type"], RemoteCommentVerificationFailedEvent>;
+    EventAction<SubscribedEvent>
+    | EventAction<PingEvent>
+    | EventAction<ProfileUpdatedEvent>
+    | EventAction<NodeSettingsChangedEvent>
+    | EventAction<ClientSettingsChangedEvent>
+    | EventAction<RegisteredNameOperationStatusEvent>
+    | EventAction<NodeNameChangedEvent>
+    | EventAction<FeedStatusUpdatedEvent>
+    | EventAction<StoriesStatusUpdatedEvent>
+    | EventAction<RemoteNodeFullNameChangedEvent>
+    | EventAction<PeopleChangedEvent>
+    | EventAction<RemoteNodeAvatarChangedEvent>
+    | EventAction<PostingAddedEvent>
+    | EventAction<PostingUpdatedEvent>
+    | EventAction<PostingDeletedEvent>
+    | EventAction<PostingRestoredEvent>
+    | EventAction<PostingReactionsChangedEvent>
+    | EventAction<PostingCommentsChangedEvent>
+    | EventAction<DraftAddedEvent>
+    | EventAction<DraftUpdatedEvent>
+    | EventAction<DraftDeletedEvent>
+    | EventAction<RemotePostingVerifiedEvent>
+    | EventAction<RemotePostingVerificationFailedEvent>
+    | EventAction<RemoteReactionAddedEvent>
+    | EventAction<RemoteReactionDeletedEvent>
+    | EventAction<RemoteReactionVerifiedEvent>
+    | EventAction<RemoteReactionVerificationFailedEvent>
+    | EventAction<StoryAddedEvent>
+    | EventAction<StoryDeletedEvent>
+    | EventAction<StoryUpdatedEvent>
+    | EventAction<SubscriberAddedEvent>
+    | EventAction<SubscriberDeletedEvent>
+    | EventAction<SubscriptionAddedEvent>
+    | EventAction<SubscriptionDeletedEvent>
+    | EventAction<CommentAddedEvent>
+    | EventAction<CommentUpdatedEvent>
+    | EventAction<CommentDeletedEvent>
+    | EventAction<CommentReactionsChangedEvent>
+    | EventAction<RemoteCommentAddedEvent>
+    | EventAction<RemoteCommentUpdatedEvent>
+    | EventAction<RemoteCommentDeletedEvent>
+    | EventAction<RemoteCommentVerifiedEvent>
+    | EventAction<RemoteCommentVerificationFailedEvent>;
