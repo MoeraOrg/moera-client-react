3c6c2a0f495edcba87f0edd52d54626d01c3e61c
Shmuel Melamud <smelamud@redhat.com>
2021-07-19 03:26:40 +0300

    Converted fetchTimeout() to TS.

--- src/api/fetch-timeout.ts
+++ src/api/fetch-timeout.ts
@@ -1,4 +1,5 @@
-import { retry } from "redux-saga/effects";
+import { retry } from 'typed-redux-saga/macro';
+import { CallEffect } from "redux-saga/effects";
 
 const FETCH_TIMEOUT = 10000; // ms
 const UPDATE_TIMEOUT = 300000; // ms
@@ -6,7 +7,7 @@ const RETRY_DELAY = 1000; // ms
 const RETRY_LIMIT = 3;
 
 // Source: https://stackoverflow.com/a/57888548/5541719
-export function fetchTimeout(url, ms, { signal, ...options } = {}) {
+export function fetchTimeout(url: string, ms: number, { signal, ...options }: RequestInit = {}): Promise<Response> {
     const controller = new AbortController();
     const promise = fetch(url, { signal: controller.signal, ...options });
     if (signal) {
@@ -16,7 +17,7 @@ export function fetchTimeout(url, ms, { signal, ...options } = {}) {
     return promise.finally(() => clearTimeout(timeout));
 }
 
-export function* retryFetch(url, options) {
+export function* retryFetch(url: string, options: RequestInit): Generator<CallEffect<Response>, Response> {
     let limit;
     let timeout;
     if (options.method === "POST" || options.method === "PUT") {
@@ -26,5 +27,5 @@ export function* retryFetch(url, options) {
         limit = RETRY_LIMIT;
         timeout = FETCH_TIMEOUT;
     }
-    return yield retry(limit, RETRY_DELAY, fetchTimeout, url, timeout, options);
+    return yield *retry(limit, RETRY_DELAY, fetchTimeout, url, timeout, options);
 }
