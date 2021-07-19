7bd5d785bde3b39f421a51766a75d299a18a3f65
Shmuel Melamud <smelamud@redhat.com>
2021-07-20 02:44:21 +0300

    Converted ShareDialog to TS.

--- src/ui/sharedialog/ShareDialog.tsx
+++ src/ui/sharedialog/ShareDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import { createSelector } from 'reselect';
 
 import { Button, ModalDialog } from "ui/control";
@@ -7,16 +7,19 @@ import { closeShareDialog, shareDialogCopyLink } from "state/sharedialog/actions
 import { SOCIAL_BUTTONS, SOCIAL_BUTTONS_ORDER } from "ui/sharedialog/social-buttons";
 import SocialButton from "ui/sharedialog/SocialButton";
 import { getSetting } from "state/settings/selectors";
+import { ClientState } from "state/state";
 import "./ShareDialog.css";
 
-const ShareDialog = ({show, title, url, socialButtons, closeShareDialog, shareDialogCopyLink}) => (
-    show &&
+type Props = ConnectedProps<typeof connector>;
+
+const ShareDialog = ({show, title, url, socialButtons, closeShareDialog, shareDialogCopyLink}: Props) => (
+    show ?
         <ModalDialog title="Share" className="share-dialog" onClose={closeShareDialog}>
             <div className="modal-body">
                 <div className="title">"{title}"</div>
                 <div className="link">
-                    <input type="text" className="form-control" value={url} onChange={() => {}}/>
-                    <Button variant="secondary" onClick={() => shareDialogCopyLink(url)}>Copy</Button>
+                    <input type="text" className="form-control" value={url ?? ""} onChange={() => {}}/>
+                    <Button variant="secondary" onClick={() => shareDialogCopyLink(url ?? "")}>Copy</Button>
                 </div>
                 <div className="social">
                     {socialButtons.map(type => <SocialButton key={type} type={type} url={url} title={title}/>)}
@@ -26,25 +29,29 @@ const ShareDialog = ({show, title, url, socialButtons, closeShareDialog, shareDi
                 <Button variant="primary" onClick={closeShareDialog}>Close</Button>
             </div>
         </ModalDialog>
+    :
+        null
 );
 
 const getSocialButtons = createSelector(
-    state => getSetting(state, "share.social-buttons.usage"),
+    (state: ClientState) => getSetting(state, "share.social-buttons.usage") as any as Record<string, number>,
     usage => (
         [...SOCIAL_BUTTONS].sort((a, b) => {
             const ua = usage[a] ?? 0;
             const ub = usage[b] ?? 0;
-            return ua !== ub ? ub - ua : SOCIAL_BUTTONS_ORDER.get(a) - SOCIAL_BUTTONS_ORDER.get(b);
+            return ua !== ub ? ub - ua : SOCIAL_BUTTONS_ORDER.get(a)! - SOCIAL_BUTTONS_ORDER.get(b)!;
         })
     )
 );
 
-export default connect(
-    state => ({
+const connector = connect(
+    (state: ClientState) => ({
         show: state.shareDialog.show,
         title: state.shareDialog.title,
         url: state.shareDialog.url,
         socialButtons: getSocialButtons(state)
     }),
     { closeShareDialog, shareDialogCopyLink }
-)(ShareDialog);
+);
+
+export default connector(ShareDialog);
