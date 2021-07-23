8aa1595197973c6de341960d749bb19e1b1cb246
Shmuel Melamud <smelamud@redhat.com>
2021-07-23 04:45:07 +0300

    Converted NumberField to TS with hooks.

---
+++ src/ui/control/field/NumberField.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NumberPicker } from 'react-widgets';
+
+import { useUndoableField } from "ui/control/field/undoable-field";
+import { FormGroup } from "ui/control/FormGroup";
+import "./NumberField.css";
+
+interface Props {
+    name: string;
+    title?: string;
+    autoFocus?: boolean;
+    disabled?: boolean;
+    format?: string;
+    min?: number;
+    max?: number;
+    step?: number;
+    initialValue?: number;
+    defaultValue?: number;
+}
+
+export const NumberField = ({name, title, autoFocus, disabled, format, min, max, step, initialValue,
+                             defaultValue}: Props) => {
+    const [{value}, {touched, error}, {setValue}, {undo, reset, onUndo, onReset}] =
+        useUndoableField<number>(name, initialValue, defaultValue);
+
+    return (
+        <FormGroup
+            title={title}
+            name={name}
+            undo={undo}
+            reset={reset}
+            onUndo={onUndo}
+            onReset={onReset}
+        >
+            <>
+                {/* <label> is not functional, because NumberPicker doesn't allow to set id */}
+                <div className="number-control">
+                    <NumberPicker
+                        name={name}
+                        value={value}
+                        onChange={v => v != null && setValue(v)}
+                        autoFocus={autoFocus}
+                        disabled={disabled}
+                        format={format}
+                        min={min}
+                        max={max}
+                        step={step ?? 1}
+                    />
+                </div>
+                {touched && error && <div className="invalid-feedback">{error}</div>}
+            </>
+        </FormGroup>
+    );
+}
