4e25622218d827433c4079ae9066dbe9948cfdfb
Shmuel Melamud <smelamud@redhat.com>
2021-08-09 01:30:15 +0300

    Turn off request timeout for POST/PUT requests sending a file or a large body.

--- src/api/fetch-timeout.ts
+++ src/api/fetch-timeout.ts
@@ -2,30 +2,38 @@ import { retry } from 'typed-redux-saga/macro';
 import { CallEffect } from 'redux-saga/effects';
 
 const FETCH_TIMEOUT = 10000; // ms
-const UPDATE_TIMEOUT = 300000; // ms
+const UPDATE_TIMEOUT = 60000; // ms
 const RETRY_DELAY = 1000; // ms
 const RETRY_LIMIT = 3;
+const LARGE_BODY_MIN = 65536;
 
 // Source: https://stackoverflow.com/a/57888548/5541719
-export function fetchTimeout(url: string, ms: number, { signal, ...options }: RequestInit = {}): Promise<Response> {
+export function fetchTimeout(url: string, timeoutMs: number | null,
+                             { signal, ...options }: RequestInit = {}): Promise<Response> {
     const controller = new AbortController();
     const promise = fetch(url, { signal: controller.signal, ...options });
     if (signal) {
         signal.addEventListener("abort", () => controller.abort());
     }
-    const timeout = setTimeout(() => controller.abort(), ms);
-    return promise.finally(() => clearTimeout(timeout));
+    if (timeoutMs != null) {
+        const timeout = setTimeout(() => controller.abort(), timeoutMs);
+        return promise.finally(() => clearTimeout(timeout));
+    } else {
+        return promise;
+    }
 }
 
 export function* retryFetch(url: string, options: RequestInit): Generator<CallEffect<Response>, Response> {
-    let limit;
-    let timeout;
+    let limit: number;
+    let timeoutMs: number | null;
     if (options.method === "POST" || options.method === "PUT") {
         limit = 1;
-        timeout = UPDATE_TIMEOUT;
+        timeoutMs = (
+            options.body instanceof Blob || (typeof (options.body) === "string" && options.body.length > LARGE_BODY_MIN)
+        ) ? UPDATE_TIMEOUT : null;
     } else {
         limit = RETRY_LIMIT;
-        timeout = FETCH_TIMEOUT;
+        timeoutMs = FETCH_TIMEOUT;
     }
-    return yield* retry(limit, RETRY_DELAY, fetchTimeout, url, timeout, options);
+    return yield* retry(limit, RETRY_DELAY, fetchTimeout, url, timeoutMs, options);
 }
