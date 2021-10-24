004424a33056b8e9a4d05ac6df0b31e3e89a63d8
Shmuel Melamud <smelamud@redhat.com>
2021-10-24 23:53:58 +0300

    Use hook instead of Dropzone component in RichTextImageDialogDropzone.

--- src/ui/control/richtexteditor/RichTextImageDialogDropzone.tsx
+++ src/ui/control/richtexteditor/RichTextImageDialogDropzone.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import cx from 'classnames';
 
 import { ACCEPTED_IMAGE_TYPES } from "ui/image-types";
@@ -7,17 +7,16 @@ import { Button } from "ui/control/Button";
 import "./RichTextImageDialogDropzone.css";
 
 export default function RichTextImageDialogDropzone() {
+    const {getRootProps, getInputProps, isDragAccept, isDragReject} =
+        useDropzone({noClick: true, noKeyboard: true, accept: ACCEPTED_IMAGE_TYPES, maxFiles: 1});
+
     return (
-        <Dropzone noClick noKeyboard accept={ACCEPTED_IMAGE_TYPES} maxFiles={1}>
-            {({getRootProps, getInputProps, isDragAccept, isDragReject}) => (
-                <div className={cx(
-                    "rich-text-image-dialog-dropzone",
-                    {"drag-accept": isDragAccept, "drag-reject": isDragReject}
-                )} {...getRootProps()}>
-                    <Button variant="primary" size="sm">Upload image</Button> or drop it here
-                    <input {...getInputProps()}/>
-                </div>
-            )}
-        </Dropzone>
+        <div className={cx(
+            "rich-text-image-dialog-dropzone",
+            {"drag-accept": isDragAccept, "drag-reject": isDragReject}
+        )} {...getRootProps()}>
+            <Button variant="primary" size="sm">Upload image</Button> or drop it here
+            <input {...getInputProps()}/>
+        </div>
     );
 }
