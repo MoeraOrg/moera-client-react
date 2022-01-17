4cc869a2e846f28ebf69902b4f5ef3ca56ee7c7a
Shmuel Melamud <smelamud@redhat.com>
2022-01-17 20:39:17 +0200

    Allow embedding iframes from docs.google.com.

--- src/util/html.ts
+++ src/util/html.ts
@@ -33,7 +33,7 @@ const BASE_SAFE_HTML_SETTINGS: IOptions = {
     },
     allowedIframeHostnames: [
         "www.youtube.com", "www.youtube-nocookie.com", "player.vimeo.com", "www.facebook.com", "peer.tube",
-        "rumble.com", "open.spotify.com", "c.simmer.io", "itch.io", "odysee.com"
+        "rumble.com", "open.spotify.com", "c.simmer.io", "itch.io", "odysee.com", "docs.google.com"
     ],
     allowedIframeDomains: ["livejournal.com"],
     allowedStyles: {
