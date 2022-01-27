52fa669201194581ea14ebe1b4aed8ed166d9c3c
Shmuel Melamud <smelamud@redhat.com>
2022-01-27 23:53:29 +0200

    Upgraded to create-react-app 5.0.0.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,28 @@
 const { whenDev } = require("@craco/craco");
+const webpack = require("webpack");
 
 module.exports = {
     devServer: whenDev(() => ({
         headers: {'Access-Control-Allow-Origin': '*'}
-    }))
+    })),
+    webpack: {
+        configure: {
+            resolve: {
+                fallback: {
+                    process: require.resolve("process/browser"),
+                    // zlib: require.resolve("browserify-zlib"),
+                    // stream: require.resolve("stream-browserify"),
+                    // util: require.resolve("util"),
+                    buffer: require.resolve("buffer"),
+                    // assert: require.resolve("assert")
+                },
+            },
+            plugins: [
+                new webpack.ProvidePlugin({
+                    Buffer: ["buffer", "Buffer"],
+                    process: "process/browser",
+                }),
+            ],
+        },
+    }
 };
