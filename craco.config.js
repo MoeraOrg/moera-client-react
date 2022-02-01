e040829574e071c7d15ec253f5c18d7f52062e9b
Shmuel Melamud <smelamud@redhat.com>
2022-02-01 21:56:08 +0200

    Rolling back CRA upgrade to 5.0, because for some strange reason sanitize-html allowedStyles feature stopped to work properly.

--- craco.config.js
+++ craco.config.js
@@ -4,25 +4,5 @@ const webpack = require("webpack");
 module.exports = {
     devServer: whenDev(() => ({
         headers: {'Access-Control-Allow-Origin': '*'}
-    })),
-    webpack: {
-        configure: {
-            resolve: {
-                fallback: {
-                    process: require.resolve("process/browser"),
-                    // zlib: require.resolve("browserify-zlib"),
-                    // stream: require.resolve("stream-browserify"),
-                    // util: require.resolve("util"),
-                    buffer: require.resolve("buffer"),
-                    // assert: require.resolve("assert")
-                },
-            },
-            plugins: [
-                new webpack.ProvidePlugin({
-                    Buffer: ["buffer", "Buffer"],
-                    process: "process/browser",
-                }),
-            ],
-        },
-    }
+    }))
 };
