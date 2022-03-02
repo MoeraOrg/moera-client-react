b8e6aad955bbad505498c4e9fd0f259789ae4006
Eliyahu Duvidzon <eliduvid1@gmail.com>
2022-03-02 19:50:09 +0200

    * Upgrade react-scripts to v5.0.0

--- craco.config.js
+++
@@ -1,8 +0,0 @@
-const { whenDev } = require("@craco/craco");
-const webpack = require("webpack");
-
-module.exports = {
-    devServer: whenDev(() => ({
-        headers: {'Access-Control-Allow-Origin': '*'}
-    }))
-};
