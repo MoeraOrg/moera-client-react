8ccf5cd128033d0a488818a0fa883e212ea972b3
Shmuel Melamud <smelamud@redhat.com>
2022-09-14 04:15:09 +0300

    Integrated ICU message format for translations.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import ICU from 'i18next-icu';
 
 import message_en from "i18n/locales/en/message.json";
 import message_ru from "i18n/locales/ru/message.json";
@@ -13,16 +14,18 @@ const resources = {
     }
 };
 
-i18n.use(initReactI18next).init({
-    resources,
-    lng: "en",
-    fallbackLng: "en",
-    supportedLngs: ["en", "ru"],
-    ns: ['message'],
-    defaultNS: 'message',
-    interpolation: {
-        escapeValue: false, // not needed for React as it escapes by default
-    }
-});
+i18n.use(initReactI18next)
+    .use(ICU)
+    .init({
+        resources,
+        lng: "en",
+        fallbackLng: "en",
+        supportedLngs: ["en", "ru"],
+        ns: ['message'],
+        defaultNS: 'message',
+        interpolation: {
+            escapeValue: false, // not needed for React as it escapes by default
+        }
+    });
 
 export default i18n;
