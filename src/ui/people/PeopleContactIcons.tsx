ce8fb20721fbc5aa3227ffdb0552bcd270c2e297
Shmuel Melamud <smelamud@redhat.com>
2022-12-22 04:35:24 +0200

    Fixed CSS for contact icons.

--- src/ui/people/PeopleContactIcons.tsx
+++ src/ui/people/PeopleContactIcons.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 
 import { ClientState } from "state/state";
+import { getHomeOwnerGender } from "state/home/selectors";
 import { ContactState } from "state/people/state";
 import { getPeopleTab } from "state/people/selectors";
 import { Principal } from "ui/control";
 import SubscriberVisibility from "ui/people/SubscriberVisibility";
 import PeopleIconSet from "ui/people/PeopleIconSet";
 import "./PeopleContactIcons.css";
-import { getHomeOwnerGender } from "state/home/selectors";
 
 type Props = {
     contact: ContactState
@@ -24,62 +24,66 @@ function PeopleContactIcons({contact, tab, homeOwnerGender}: Props) {
         case "subscribers":
             if (contact.subscriber != null) {
                 return (
-                    <div className="icons">
+                    <>
                         <SubscriberVisibility subscriber={contact.subscriber}/>
-                        <PeopleIconSet icons={[
-                            {
-                                icon: "arrow-right-arrow-left",
-                                title: "mutually-subscribed",
-                                visible: hasSubscription
-                            },
-                            {
-                                icon: "person",
-                                title: "friend",
-                                visible: hasFriend && !hasFriendOf
-                            },
-                            {
-                                icon: "person-walking-arrow-right",
-                                title: "in-friends",
-                                visible: !hasFriend && hasFriendOf
-                            },
-                            {
-                                icon: "people-arrows",
-                                title: "mutual-friends",
-                                visible: hasFriend && hasFriendOf
-                            }
-                        ]}/>
-                    </div>
+                        <div className="icons">
+                            <PeopleIconSet icons={[
+                                {
+                                    icon: "arrow-right-arrow-left",
+                                    title: "mutually-subscribed",
+                                    visible: hasSubscription
+                                },
+                                {
+                                    icon: "person",
+                                    title: "friend",
+                                    visible: hasFriend && !hasFriendOf
+                                },
+                                {
+                                    icon: "person-walking-arrow-right",
+                                    title: "in-friends",
+                                    visible: !hasFriend && hasFriendOf
+                                },
+                                {
+                                    icon: "people-arrows",
+                                    title: "mutual-friends",
+                                    visible: hasFriend && hasFriendOf
+                                }
+                            ]}/>
+                        </div>
+                    </>
                 );
             }
             break;
         case "subscriptions":
             if (contact.subscription != null) {
                 return (
-                    <div className="icons">
+                    <>
                         <Principal value={contact.subscription.operations?.view ?? "public"} defaultValue="public"/>
-                        <PeopleIconSet icons={[
-                            {
-                                icon: "arrow-right-arrow-left",
-                                title: "mutually-subscribed",
-                                visible: hasSubscriber
-                            },
-                            {
-                                icon: "person",
-                                title: "friend",
-                                visible: hasFriend && !hasFriendOf
-                            },
-                            {
-                                icon: "person-walking-arrow-right",
-                                title: "in-friends",
-                                visible: !hasFriend && hasFriendOf
-                            },
-                            {
-                                icon: "people-arrows",
-                                title: "mutual-friends",
-                                visible: hasFriend && hasFriendOf
-                            }
-                        ]}/>
-                    </div>
+                        <div className="icons">
+                            <PeopleIconSet icons={[
+                                {
+                                    icon: "arrow-right-arrow-left",
+                                    title: "mutually-subscribed",
+                                    visible: hasSubscriber
+                                },
+                                {
+                                    icon: "person",
+                                    title: "friend",
+                                    visible: hasFriend && !hasFriendOf
+                                },
+                                {
+                                    icon: "person-walking-arrow-right",
+                                    title: "in-friends",
+                                    visible: !hasFriend && hasFriendOf
+                                },
+                                {
+                                    icon: "people-arrows",
+                                    title: "mutual-friends",
+                                    visible: hasFriend && hasFriendOf
+                                }
+                            ]}/>
+                        </div>
+                    </>
                 );
             }
             break;
@@ -119,33 +123,35 @@ function PeopleContactIcons({contact, tab, homeOwnerGender}: Props) {
             if (contact.friend != null) {
                 const view = contact.friend.groups?.find(fg => fg.id === tab)?.operations?.view ?? "public";
                 return (
-                    <div className="icons">
+                    <>
                         <Principal value={view} defaultValue="public"/>
-                        <PeopleIconSet icons={[
-                            {
-                                icon: "eye",
-                                title: "subscribed",
-                                gender: homeOwnerGender,
-                                visible: !hasSubscriber && hasSubscription
-                            },
-                            {
-                                icon: "arrows-to-eye",
-                                title: "subscribed-to-me",
-                                gender: contact.contact.gender,
-                                visible: hasSubscriber && !hasSubscription
-                            },
-                            {
-                                icon: "arrow-right-arrow-left",
-                                title: "mutually-subscribed",
-                                visible: hasSubscriber && hasSubscription
-                            },
-                            {
-                                icon: "people-arrows",
-                                title: "mutual-friends",
-                                visible: hasFriend
-                            }
-                        ]}/>
-                    </div>
+                        <div className="icons">
+                            <PeopleIconSet icons={[
+                                {
+                                    icon: "eye",
+                                    title: "subscribed",
+                                    gender: homeOwnerGender,
+                                    visible: !hasSubscriber && hasSubscription
+                                },
+                                {
+                                    icon: "arrows-to-eye",
+                                    title: "subscribed-to-me",
+                                    gender: contact.contact.gender,
+                                    visible: hasSubscriber && !hasSubscription
+                                },
+                                {
+                                    icon: "arrow-right-arrow-left",
+                                    title: "mutually-subscribed",
+                                    visible: hasSubscriber && hasSubscription
+                                },
+                                {
+                                    icon: "people-arrows",
+                                    title: "mutual-friends",
+                                    visible: hasFriend
+                                }
+                            ]}/>
+                        </div>
+                    </>
                 );
             }
             break;
