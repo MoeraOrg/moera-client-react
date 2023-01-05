e53c45751087c1088c79f45b9a97e8281333fa1f
Shmuel Melamud <smelamud@redhat.com>
2023-01-06 00:14:02 +0200

    Types.

--- src/state/init-selectors.ts
+++ src/state/init-selectors.ts
@@ -5,15 +5,15 @@ import { isConnectedToHome, isHomeOwnerNameSet } from "state/home/selectors";
 import { isOwnerNameSet } from "state/node/selectors";
 import { isSettingsClientValuesLoaded } from "state/settings/selectors";
 
-export function introduced(state: ClientState) {
+export function introduced(state: ClientState): boolean {
     return isAtHomeNode(state) || !isCartesRunOut(state)
         || (isCartesInitialized(state) && (!isConnectedToHome(state) || !isHomeOwnerNameSet(state)));
 }
 
-export function mutuallyIntroduced(state: ClientState) {
+export function mutuallyIntroduced(state: ClientState): boolean {
     return isOwnerNameSet(state) && introduced(state);
 }
 
-export function namingInitialized(state: ClientState) {
+export function namingInitialized(state: ClientState): boolean {
     return isSettingsClientValuesLoaded(state) || (isCartesInitialized(state) && !isConnectedToHome(state));
 }
