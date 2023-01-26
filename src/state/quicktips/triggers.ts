8ed8a5df111197e357cb0bb77707d633d5f41bc2
Shmuel Melamud <smelamud@redhat.com>
2023-01-27 01:32:48 +0200

    Reorder.

--- src/state/quicktips/triggers.ts
+++ src/state/quicktips/triggers.ts
@@ -1,9 +1,9 @@
 import { trigger } from "state/trigger";
-import { SETTINGS_CLIENT_VALUES_LOADED } from "state/settings/actions";
-import { isQuickTipsToBeShown } from "state/quicktips/selectors";
-import { CLOSE_QUICK_TIPS, closeQuickTips, OPEN_QUICK_TIPS, openQuickTips } from "state/quicktips/actions";
-import { MNEMONIC_CLOSE } from "state/nodename/actions";
 import { dialogClosed, dialogOpened } from "state/navigation/actions";
+import { SETTINGS_CLIENT_VALUES_LOADED } from "state/settings/actions";
+import { CLOSE_QUICK_TIPS, closeQuickTips, OPEN_QUICK_TIPS, openQuickTips } from "state/quicktips/actions";
+import { isQuickTipsToBeShown } from "state/quicktips/selectors";
+import { MNEMONIC_CLOSE } from "state/nodename/actions";
 
 export default [
     trigger([SETTINGS_CLIENT_VALUES_LOADED, MNEMONIC_CLOSE], isQuickTipsToBeShown, openQuickTips),
