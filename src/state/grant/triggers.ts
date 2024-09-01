1e99b7bf31020c8254aabcf36f7423016089b8ca
Shmuel Melamud <smelamud@redhat.com>
2024-09-01 04:47:21 +0300

    Display connection dialog on grant page, if not logged in yet. After that, re-validate the grant request.

--- src/state/grant/triggers.ts
+++ src/state/grant/triggers.ts
@@ -1,8 +1,12 @@
-import { conj, trigger } from "state/trigger";
+import { conj, inv, trigger } from "state/trigger";
 import { isAtGrantPage } from "state/navigation/selectors";
+import { isConnectedToHome } from "state/home/selectors";
+import { openConnectDialog } from "state/connectdialog/actions";
 import { isGrantToBeValidated } from "state/grant/selectors";
 import { grantValidate } from "state/grant/actions";
 
 export default [
-    trigger("GO_TO_PAGE", conj(isAtGrantPage, isGrantToBeValidated), grantValidate)
+    trigger("GO_TO_PAGE", conj(isAtGrantPage, isGrantToBeValidated), grantValidate),
+    trigger("GO_TO_PAGE", conj(isAtGrantPage, inv(isConnectedToHome)), openConnectDialog),
+    trigger("CONNECTED_TO_HOME", isAtGrantPage, grantValidate)
 ];
--- src/ui/grant/GrantContent.tsx
+++ src/ui/grant/GrantContent.tsx
@@ -6,16 +6,18 @@ import GrantConfirmed from "ui/grant/GrantConfirmed";
 import GrantValidating from "ui/grant/GrantValidating";
 import GrantNotValid from "ui/grant/GrantNotValid";
 import GrantConfirmation from "ui/grant/GrantConfirmation";
+import { isConnectedToHome } from "state/home/selectors";
 
 export default function GrantContent() {
     const validated = useSelector((state: ClientState) => state.grant.validated);
     const valid = useSelector((state: ClientState) => state.grant.valid);
     const confirmed = useSelector((state: ClientState) => state.grant.confirmed);
+    const connectedToHome = useSelector(isConnectedToHome);
 
     if (confirmed) {
         return <GrantConfirmed/>;
     }
-    if (!validated) {
+    if (!validated || !connectedToHome) {
         return <GrantValidating/>;
     }
     if (!valid) {
