24d7c5f564d3bf84131771e387e61170b0222440
Shmuel Melamud <smelamud@redhat.com>
2024-12-09 20:24:24 +0200

    Better design for special blocks in the visual editor.

--- src/ui/control/richtexteditor/visual/VisualRenderElement.tsx
+++ src/ui/control/richtexteditor/visual/VisualRenderElement.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { DefaultElement, RenderElementProps } from 'slate-react';
 
 import { isScriptureElement } from "ui/control/richtexteditor/visual/scripture";
 
 export default function VisualRenderElement(props: RenderElementProps) {
     const {element, attributes, children} = props;
+
+    const {t} = useTranslation();
 
     if (isScriptureElement(element)) {
         switch (element.type) {
@@ -15,7 +18,13 @@ export default function VisualRenderElement(props: RenderElementProps) {
             case "spoiler":
                 return <span className="spoiler" {...attributes}>{children}</span>;
             case "spoiler-block":
-                return <div className="spoiler" {...attributes}>{children}</div>;
+                return (
+                    // @ts-ignore
+                    <div className="block-border block-border-gray"  style={{"--bb-badge": `"${t("spoiler")}"`}}
+                         {...attributes}>
+                        {children}
+                    </div>
+                );
             case "mention":
                 return <span className="mention" {...attributes}>{children}</span>;
             case "horizontal-rule":
@@ -38,8 +47,10 @@ export default function VisualRenderElement(props: RenderElementProps) {
                 );
             case "details":
                 return (
-                    <details open {...attributes}>
-                        {element.summary && <summary>{element.summary}</summary>}
+                    // @ts-ignore
+                    <details open className="block-border block-border-blue" style={{"--bb-badge": `"${t("fold")}"`}}
+                             {...attributes}>
+                        {element.summary && <summary contentEditable={false}>{element.summary}</summary>}
                         {children}
                     </details>
                 );
