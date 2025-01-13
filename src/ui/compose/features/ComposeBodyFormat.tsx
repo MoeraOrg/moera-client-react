ab0b61071fb52cc9634db80a7be346670a670e27
Shmuel Melamud <smelamud@redhat.com>
2025-01-14 01:41:42 +0200

    Tabs can be scrolled horizontally if they do not fit the screen size.

--- src/ui/compose/features/ComposeBodyFormat.tsx
+++ src/ui/compose/features/ComposeBodyFormat.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
 import { SourceFormat } from "api";
 import { SelectField, SelectFieldChoice } from "ui/control/field";
@@ -9,13 +8,11 @@ interface Props {
 }
 
 export default function ComposeBodyFormat({sourceFormats}: Props) {
-    const {t} = useTranslation();
-
     const choices: SelectFieldChoice[] = sourceFormats
         .filter(f => f !== "application")
         .map(value => ({value, title: `source-format.${value.replaceAll("/", "--")}`}));
     return (
-        <SelectField title={t("text-formatting")} name="bodyFormat" horizontal layout="left" groupClassName="ps-2"
-                     col="col-md-3" choices={choices} anyValue/>
+        <SelectField name="bodyFormat" horizontal layout="left" groupClassName="ps-2" col="col-md-3" choices={choices}
+                     anyValue/>
     );
 }
