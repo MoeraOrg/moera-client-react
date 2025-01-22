cb24f6a2a08046e5d011dcdc6e351e8eefbdc63b
Shmuel Melamud <smelamud@redhat.com>
2025-01-23 00:35:14 +0200

    Possibly the app on some versions of Android does not report "Android" in the user agent string.

--- src/ui/browser.ts
+++ src/ui/browser.ts
@@ -87,7 +87,7 @@ export const isDevMode = (): boolean =>
     !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
 export const isTouchScreen = (): boolean =>
-    userAgentOs === "android" || userAgentOs === "ios";
+    userAgentOs === "android" || userAgentOs === "ios" || isAndroidApp();
 
 export const isMobile = (): boolean =>
     userAgentOs === "android" || userAgentOs === "ios";
