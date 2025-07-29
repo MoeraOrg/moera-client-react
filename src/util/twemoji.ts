1aaf0f643502b09abb6cf3b962f30296d79a32cc
Shmuel Melamud <smelamud@redhat.com>
2025-07-30 01:42:27 +0300

    Updated Twemoji design.

--- src/util/twemoji.ts
+++ src/util/twemoji.ts
@@ -1,3 +1,3 @@
 export function twemojiUrl(emoji: string): string {
-    return `https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/${emoji}.svg`;
+    return `https://cdnjs.cloudflare.com/ajax/libs/twemoji/15.0.3/svg/${emoji}.svg`;
 }
