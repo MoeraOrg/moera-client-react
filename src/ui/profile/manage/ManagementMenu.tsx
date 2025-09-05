96558f81ed03ee47a807713084a79abe52ad59c7
Shmuel Melamud <smelamud@redhat.com>
2025-09-05 16:56:48 +0300

    Sharing and settings in ManagementMenu.

--- src/ui/profile/manage/ManagementMenu.tsx
+++ src/ui/profile/manage/ManagementMenu.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { ClientState } from "state/state";
 import { nodeNameUpdateDialog, registerNameDialog } from "state/nodename/actions";
 import { isNodeNameDefined, isNodeNameManageable, isNodeNameOperationPending } from "state/nodename/selectors";
+import { shareDialogPrepare, sharePageCopyLink } from "state/sharedialog/actions";
 import { DropdownMenu, MenuButton } from "ui/control";
 import RegisterNameDialog from "ui/profile/manage/RegisterNameDialog";
 import { REL_CURRENT } from "util/rel-node-name";
@@ -21,30 +22,59 @@ export default function ManagementMenu() {
     const dispatch = useDispatch();
     const {t} = useTranslation();
 
+    const onCopyLink = () => dispatch(sharePageCopyLink(REL_CURRENT, "/"));
+    const onShare = () => dispatch(shareDialogPrepare(REL_CURRENT, "/"));
+
     return (
         <>
             <DropdownMenu items={[
                 {
                     title: t("update-current-name"),
                     nodeName: REL_CURRENT,
-                    href: "/profile",
+                    href: "/",
                     onClick: () => dispatch(nodeNameUpdateDialog(false)),
                     show: nameDefined && manageable && !operationPending
                 },
                 {
                     title: nameDefined ? t("register-different-name") : t("register-new-name"),
                     nodeName: REL_CURRENT,
-                    href: "/profile",
+                    href: "/",
                     onClick: () => dispatch(registerNameDialog()),
                     show: manageable && !operationPending
                 },
                 {
                     title: t("transfer-existing-name"),
                     nodeName: REL_CURRENT,
-                    href: "/profile",
+                    href: "/",
                     onClick: () => dispatch(nodeNameUpdateDialog(true)),
                     show: manageable && !operationPending
-                }
+                },
+                {
+                    divider: true
+                },
+                {
+                    title: t("copy-link"),
+                    nodeName: REL_CURRENT,
+                    href: "/",
+                    onClick: onCopyLink,
+                    show: true
+                },
+                {
+                    title: t("share"),
+                    nodeName: REL_CURRENT,
+                    href: "/",
+                    onClick: onShare,
+                    show: true
+                },
+                {
+                    divider: true
+                },
+                {
+                    title: t("settings"),
+                    nodeName: REL_CURRENT,
+                    href: "/settings",
+                    show: true
+                },
             ]} className="management-menu">
                 <MenuButton active/>
             </DropdownMenu>
