8fb3760ece99ba76508142a34c0c8ed9c3c5ec41
Shmuel Melamud <smelamud@redhat.com>
2025-10-01 01:59:59 +0300

    Adjust the scrolling position when the shadow appears.

--- src/ui/mainmenu/scroll-shadow.ts
+++ src/ui/mainmenu/scroll-shadow.ts
@@ -1,4 +1,7 @@
-import { RefObject, useCallback, useState } from "react";
+import { RefObject, useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { isConnectedToHome } from "state/home/selectors";
 import { useIntersect } from "ui/hook";
 
 interface ScrollShadow {
@@ -7,6 +10,7 @@ interface ScrollShadow {
 }
 
 export function useScrollShadow(): ScrollShadow {
+    const connectedToHome = useSelector(isConnectedToHome);
     const [shadow, setShadow] = useState<boolean>(false);
 
     const onIntersect = useCallback(
@@ -14,7 +18,8 @@ export function useScrollShadow(): ScrollShadow {
         [setShadow]
     );
 
-    const sentinel = useIntersect(onIntersect);
+    const margin = connectedToHome ? 50 : 100;
+    const sentinel = useIntersect(onIntersect, {rootMargin: `-${margin}px 0px 0px 0px`});
 
     return {shadow, sentinel};
 }
