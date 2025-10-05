e10cb6631d9bf5d7b0270d4aba6a4908514f9d2c
Shmuel Melamud <smelamud@redhat.com>
2025-10-05 03:58:12 +0300

    Extra import.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,7 +6,6 @@ import { isAtNode, isNodeIntroduced } from "state/node/selectors";
 import { isConnectedToHome } from "state/home/selectors";
 import { isAtGrantPage, isAtRemovalPage } from "state/navigation/selectors";
 import { getFeedWidth, getSetting } from "state/settings/selectors";
-import { useReload } from "ui/hook/reload";
 import EventsFrontend from "ui/events/EventsFrontend";
 import Navigation from "ui/navigation/Navigation";
 import ErrorPane from "ui/error/ErrorPane";
