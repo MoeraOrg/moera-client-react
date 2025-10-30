0cd0d860f148e033ba58ce8bafa57281f8ad9467
Shmuel Melamud <smelamud@redhat.com>
2025-10-30 05:35:36 +0200

    Sheriff's node name is the same for the developer's configuration. It should be registered on the dev naming server.

--- src/sheriffs.ts
+++ src/sheriffs.ts
@@ -1,5 +1,3 @@
-import * as Browser from "ui/browser";
-
-export const SHERIFF_GOOGLE_PLAY_TIMELINE = Browser.isDevMode() ? "alice-dev_0" : "google-play-sheriff_0";
+export const SHERIFF_GOOGLE_PLAY_TIMELINE = "google-play-sheriff_0";
 
 export const SHERIFF_USER_LIST_HIDE = "sheriff-hide";
