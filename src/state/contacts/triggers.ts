e4c2de03d5df74054713c21612add019d281236f
Shmuel Melamud <smelamud@redhat.com>
2025-10-31 03:28:40 +0200

    Load a default set of suggested nodes even when we are not signed in.

--- src/state/contacts/triggers.ts
+++ src/state/contacts/triggers.ts
@@ -1,13 +1,11 @@
 import { trigger } from "state/trigger";
 import { contactsLoad, ContactsPrepareAction } from "state/contacts/actions";
 import { isContactsQueryToBeLoaded } from "state/contacts/selectors";
-import { isConnectedToHome } from "state/home/selectors";
 
 export default [
     trigger(
         "CONTACTS_PREPARE",
-        (state, signal: ContactsPrepareAction) =>
-            isConnectedToHome(state) && isContactsQueryToBeLoaded(state, signal.payload.query),
+        (state, signal: ContactsPrepareAction) => isContactsQueryToBeLoaded(state, signal.payload.query),
         signal => contactsLoad(signal.payload.query)
     )
 ];
